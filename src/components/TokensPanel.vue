07cef2b0884051b3f0468f5b1cbaa98da183e584
janmichek <jan@janmichek.cz>
2023-04-04 03:26:47 +0200

    feat: print tokens values in simple form

---
+++ src/components/TokensPanel.vue
@@ -0,0 +1,36 @@
+<template>
+  <app-panel class="tokens-panel">
+    <table>
+      <tr>
+        <th>a</th>
+      </tr>
+    </table>
+  </app-panel>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.tokens-panel {
+    padding: var(--space-4) var(--space-1);
+
+    @media (--desktop) {
+        padding: var(--space-4);
+    }
+
+    &__table {
+        display: none;
+        @media (--desktop) {
+            display: revert;
+        }
+    }
+
+    &__table-condensed {
+        @media (--desktop) {
+            display: none;
+        }
+    }
+}
+</style>
---
+++ src/pages/tokens/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tokens">
+    <Head>
+      <Title>{{ APP_TITLE_SHORT }} | Tokens</Title>
+    </Head>
+    <div class="tokens__parallax">
+      <page-header>
+        Tokens
+      </page-header>
+      <tokens-panel class="tokens__panel"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import tokensPanel from '@/components/tokensPanel'
+import PageHeader from '@/components/PageHeader'
+
+export default {
+  name: 'TransactionsView',
+  components: { PageHeader, tokensPanel },
+}
+</script>
+
+<style scoped>
+.tokens {
+    background-image: url("@/assets/background.svg");
+    background-color: var(--color-midnight-35);
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    &__parallax {
+        padding: 120px var(--space-1) var(--space-6);
+        max-width: var(--container-width);
+        margin: 0 auto;
+        @media (--desktop) {
+            padding: 120px 0;
+        }
+    }
+}
+</style>

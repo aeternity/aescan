c0d32bf49487a424b0e5185b2fa1394e87d4cb32
luekromanowicz <lromanowicz1@gmail.com>
2023-04-11 16:03:33 +0200

    clear previous tokenHolders when loading next page

--- src/stores/tokenDetails.js
+++ src/stores/tokenDetails.js
@@ -74,6 +74,7 @@ export const useTokenDetailsStore = defineStore('tokenDetails', () => {
   }
 
   async function fetchTokenHolders({ queryParameters, limit } = {}) {
+    rawTokenHolders.value = null
     const defaultParameters = `/v2/aex9/${tokenId.value}/balances?limit=${limit ?? 10}`
     const { data } = await axios.get(`${useRuntimeConfig().public.MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
     rawTokenHolders.value = data

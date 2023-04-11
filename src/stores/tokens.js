299ea7914d0db3586f539435f6b1545d14a26a18
janmichek <jan@janmichek.cz>
2023-04-11 13:47:25 +0200

    chore: rewrite store to Composition API

--- src/stores/tokens.js
+++ src/stores/tokens.js
@@ -1,25 +1,30 @@
-import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import axios from 'axios'
 import { useRuntimeConfig } from 'nuxt/app'
+import { defineStore } from 'pinia'
 
-export const useTokensStore = defineStore('tokens', {
-  state: () => ({
-    listedTokens: null,
-    allTokens: null,
-  }),
+export const useTokensStore = defineStore('tokens', () => {
+  const listedTokens = ref(null)
+  const allTokens = ref(null)
 
-  actions: {
-    async fetchAllTokens(queryParameters = null) {
-      this.allTokens = null
-      const { data } = await axios.get(`${useRuntimeConfig().public.MIDDLEWARE_URL}${queryParameters || '/v2/aex9?by=name&direction=forward'}`)
-      this.allTokens = data
-    },
-    getListedTokens() {
-      this.listedTokens = {
-        next: null,
-        prev: null,
-        data: LISTED_TOKENS,
-      }
-    },
-  },
+  async function fetchAllTokens(queryParameters = null) {
+    allTokens.value = null
+    const { data } = await axios.get(`${useRuntimeConfig().public.MIDDLEWARE_URL}${queryParameters || '/v2/aex9?by=name&direction=forward'}`)
+    allTokens.value = data
+  }
+
+  function getListedTokens() {
+    listedTokens.value = {
+      next: null,
+      prev: null,
+      data: LISTED_TOKENS,
+    }
+  }
+
+  return {
+    listedTokens,
+    allTokens,
+    fetchAllTokens,
+    getListedTokens,
+  }
 })

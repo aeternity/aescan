b8204f8e06a48e7d2ed7e982da8efc0eaadf440c
luekromanowicz <lromanowicz1@gmail.com>
2023-04-11 15:44:15 +0200

    feat(4): token holders on token details page

---
+++ src/components/TokenHoldersTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Account</th>
+        <th>Amount</th>
+        <th>Percentage</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="holder in tokenHolders.data"
+        :key="holder.address">
+        <td>
+          <value-hash-ellipsed
+            :hash="holder.address"
+            :link-to="`/accounts/${holder.address}`"/>
+        </td>
+        <td>
+          {{ formatNumber(holder.amount) }} {{ tokenDetails.symbol }}
+        </td>
+        <td>
+          <template v-if="holder.percentage >= 0.00001">
+            {{ formatNumber(holder.percentage) }}
+          </template>
+          <template v-else>
+            ~0
+          </template>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  tokenHolders: {
+    type: Object,
+    required: true,
+  },
+  tokenDetails: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -337,3 +337,17 @@ export function adaptTokenDetails(token, totalSupply = null, price = null) {
 
   return tokenDetails
 }
+
+export function adaptTokenHolders(tokenHolders, tokenDetails) {
+  const formattedData = tokenHolders.data.map(holder => ({
+    address: holder.account_id,
+    amount: holder.amount / (10 ** tokenDetails.decimals),
+    percentage: (holder.amount / (10 ** (tokenDetails.decimals - 2))) / tokenDetails.totalSupply,
+  }))
+
+  return {
+    next: tokenHolders.next,
+    data: formattedData,
+    prev: tokenHolders.prev,
+  }
+}

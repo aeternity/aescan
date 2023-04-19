506e33cca37e8bfb96580a8d737bc488b6d3afaf
janmichek <jan@janmichek.cz>
2023-04-19 10:08:55 +0200

    add N/A value for empty data

--- src/components/TokenEventsDataCell.vue
+++ src/components/TokenEventsDataCell.vue
@@ -5,23 +5,26 @@
       <app-chip>{{ args[1] }}</app-chip>
       {{ data }}
     </div>
-    <div v-if="name === 'Burn'">
+    <div v-else-if="name === 'Burn'">
       {{ args[0] }}
       <app-chip>{{ args[1] }}</app-chip>
       {{ data }}
     </div>
-    <div v-if="name === 'Transfer'">
+    <div v-else-if="name === 'Transfer'">
       {{ args[0] }}
       <transaction-arrow-right-icon/>
       <app-chip>{{ args[1] }}</app-chip>
       {{ args[2] }}
     </div>
-    <div v-if="name === 'Allowance'">
+    <div v-else-if="name === 'Allowance'">
       {{ args[0] }}
       <transaction-arrow-right-icon/>
       <app-chip>{{ args[1] }}</app-chip>
       {{ args[2] }}
       {{ data }}
+    </div>
+    <div v-else>
+      N/A
     </div>
   </div>
 </template>

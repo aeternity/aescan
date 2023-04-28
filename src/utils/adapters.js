6a0e0955ecb1283023f829727862d2e1737d4aaf
Łukasz Romanowicz <46789227+lukeromanowicz@users.noreply.github.com>
2023-04-28 15:29:45 +0200

    feat: oracle details page (#169)
    Co-authored-by: Jan Michek <15363559+janmichek@users.noreply.github.com>

--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -378,9 +378,38 @@ export function adaptOracles(oracles, blockHeight) {
       queryFee: formatAettosToAe(oracle.query_fee),
     }
   })
+
   return {
     next: oracles.next,
     data: formattedData,
     prev: oracles.prev,
   }
+}
+
+export function adaptOracleDetails(oracle, creationTx, lastExtendedTx, lastQueryTx, blockHeight) {
+  const oracleDetails = {
+    id: oracle.oracle,
+    fee: formatAettosToAe(oracle.query_fee),
+    expiration: formatBlockDiffAsDatetime(
+      oracle.expire_height,
+      blockHeight,
+    ),
+    expirationHeight: oracle.expire_height,
+    registered: oracle.active_from
+      ? formatBlockDiffAsDatetime(
+        oracle.active_from,
+        blockHeight,
+      )
+      : null,
+    registeredHeight: oracle.active_from,
+    queryFormat: oracle.format.query,
+    responseFormat: oracle.format.response,
+    creator: creationTx?.tx.account_id,
+    lastExtended: lastExtendedTx ? DateTime.fromMillis(lastExtendedTx.micro_time) : null,
+    lastExtendedHeight: lastExtendedTx?.block_height,
+    lastQuery: lastQueryTx ? DateTime.fromMillis(lastQueryTx.micro_time) : null,
+    lastQueryHeight: lastQueryTx?.block_height,
+  }
+
+  return oracleDetails
 }

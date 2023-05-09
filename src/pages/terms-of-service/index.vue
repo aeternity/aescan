8607aabeb5f1ef81a72d9f8551325069e1cf6cc3
janmichek <jan@janmichek.cz>
2023-05-09 16:14:37 +0200

    add updated plain content

---
+++ src/pages/terms-of-service/index.vue
@@ -0,0 +1,272 @@
+<template>
+  <Head>
+    <Title>{{ APP_TITLE_SHORT }} | Terms of Service</Title>
+  </Head>
+  <div class="terms-of-service">
+    <h1 class="terms-of-service__heading">
+      Terms of Service
+    </h1>
+
+    <p class="terms-of-service__paragraph">
+      PLEASE READ THESE TERMS OF SERVICE CAREFULLY
+    </p>
+
+    <p class="terms-of-service__paragraph">
+      Terms and Conditions last updated on May 9th 2023
+    </p>
+    <!--      todo relative date-->
+    <p class="terms-of-service__paragraph">
+      BY ACCESSING OR USING æScan SERVICES, YOU AGREE TO BE BOUND BY THESE TERMS OF SERVICE AND ALL TERMS
+      INCORPORATED BY REFERENCE
+    </p>
+
+    <p class="terms-of-service__paragraph">
+      These Terms of Service and any terms expressly incorporated herein (“Terms”) apply to your access to and use
+      of all services (our “Services”)
+      provided by Aeternity Establishment, Dr. Grass Str. 12, FL-9490 Vaduz, PEID / Steuernummer: 2528358,
+      Vat-Nr.: 60 351 (“Company,” “we,” or
+      “us”).
+    </p>
+
+    <ol>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Use of æScan
+        </h2>
+        <p class="terms-of-service__paragraph">
+          æScan is a blockchain explorer for the æternity blockchain, and it provides users with the ability
+          to
+          view information about transactions,
+          addresses, and other data on the blockchain. By using æScan, you agree to comply with these terms
+          and conditions, as well as any other
+          policies or guidelines that may be posted on the site.
+        </p>
+        <p class="terms-of-service__paragraph">
+          You may not use æScan for any illegal or unauthorized purpose, and you agree to comply with all
+          applicable laws and regulations. Any
+          violation of these terms and conditions may result in the termination of your access to æScan.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Ownership of Content
+        </h2>
+        <p class="terms-of-service__paragraph">
+          All content on æScan, including text, graphics, logos, and software, is the property of Aeternity
+          Establishment or its licensors and is
+          protected by copyright, trademark, and other intellectual property laws. You may not copy, modify,
+          distribute, or create derivative works
+          based on any content present on æScan without prior written consent.
+        </p>
+        <p class="terms-of-service__paragraph">
+          By using æScan, you agree not to reproduce, duplicate, copy, sell, resell, or exploit any portion of
+          the site for any commercial purposes.
+          You also agree not to use any robots, spiders, or other automated means to access or collect data
+          from the site
+        </p>
+        <p class="terms-of-service__paragraph">
+          Additionally, you must not:
+        </p>
+        <p class="terms-of-service__paragraph">
+          -Use æScan in any manner that could interfere with, disrupt, negatively affect or inhibit other
+          users
+          from fully enjoying æScan, or that
+          could damage, disable, overburden or impair the functioning of our Services in any manner;
+          - Engage in unless such Automated Data Collection is confined solely to search indexing forAutomated
+          Data Collection (scraping)
+          display on the Internet
+          - Engage in the reproduction of any content posted (such as public labels or name tags) or extracted
+          from æScan’s APIs, CSV exports, or
+          our website or any of our affiliate websites without our prior consent or authorization.
+          - Attempt to circumvent any content filtering techniques employed, or attempt to access any service
+          or area of æScan that you are not
+          authorized to access;
+          - Introduce to æScan any virus, trojan worms, logic bombs, or other harmful material;
+          - Provide false, inaccurate, or misleading information; and
+          - Encourage or induce any third party to engage in any of the activities prohibited under this
+          Section.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Accuracy of Information
+        </h2>
+        <p class="terms-of-service__paragraph">
+          æScan strives to provide accurate and up-to-date information on the æternity blockchain, but there
+          are no definitive guarantees as to the
+          accuracy, completeness, or reliability of any information on the site. You use æScan at your own
+          risk.
+        </p>
+        <p class="terms-of-service__paragraph">
+          Aeternity Establishment is not responsible for any errors or omissions in the information on the
+          site, or for any loss or damage that may
+          arise from your use of the site or its content. You should always conduct your own research and
+          verification before making any decisions
+          based on the information provided on æScan.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Limitation of Liability
+        </h2>
+        <p class="terms-of-service__paragraph">
+          In no event shall Aeternity Establishment or its affiliates, directors, officers, employees, or
+          agents be liable for any direct, indirect,
+          incidental, special, or consequential damages arising out of or in connection with your use of
+          æScan.
+        </p>
+        <p class="terms-of-service__paragraph">
+          This includes but is not limited to, damages for loss of profits, goodwill, use, data, or other
+          intangible losses resulting from your use of or
+          inability to use the site, or from any unauthorized access to or alteration of your transmissions or
+          data.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Indemnification
+        </h2>
+        <p class="terms-of-service__paragraph">
+          You agree to indemnify and hold harmless Aeternity Establishment and its affiliates, directors,
+          officers, employees, and agents from and
+          against any and all claims, liabilities, damages, losses, or expenses, including reasonable
+          attorneys' fees, arising out of or in connection
+          with your use of æScan.
+        </p>
+        <p class="terms-of-service__paragraph">
+          This includes but is not limited to, any violation of these terms and conditions, or any
+          infringement
+          of any intellectual property or any other
+          right of any person or entity.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Modification of Terms and Conditions
+        </h2>
+        <p class="terms-of-service__paragraph">
+          Aeternity Establishment reserves the right to modify these terms and conditions at any time, without
+          prior notice. Your continued use of
+          æScan following any such modification constitutes your acceptance of the modified terms and
+          conditions.
+        </p>
+        <p class="terms-of-service__paragraph">
+          You are encouraged to check this page regularly for updates to these terms and conditions.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Governing Law and Jurisdiction
+        </h2>
+        <p class="terms-of-service__paragraph">
+          These terms and conditions shall be governed by and construed in accordance with the laws of the
+          Duchy of Liechtenstein, without regard
+          to its conflicts of law principles.
+        </p>
+        <p class="terms-of-service__paragraph">
+          Any legal action arising out of or in connection with your use of æScan shall be brought exclusively
+          before the courts located in
+          Liechtenstein.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Termination of Access
+        </h2>
+        <p class="terms-of-service__paragraph">
+          Aeternity Establishment reserves the right to terminate your access to æScan at any time, for any
+          reason, without prior notice to you. In
+          the event of such termination, these terms and conditions shall continue to remain in force.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          User Conduct
+        </h2>
+        <p class="terms-of-service__paragraph">
+          By using æScan, you agree to conduct yourself in a responsible and lawful manner. You agree not to
+          engage in any conduct that could be
+          considered harmful, offensive, or disruptive to other users of the site or to the functioning of the
+          site itself.
+        </p>
+        <p class="terms-of-service__paragraph">
+          This includes but is not limited to, transmitting any material that is illegal, harassing,
+          defamatory, or invasive of privacy, or engaging in any
+          activity that interferes with the proper functioning of the site, such as spamming or hacking.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Third-Party Links
+        </h2>
+        <p class="terms-of-service__paragraph">
+          æScan may contain links to third-party websites or resources that are not owned or controlled by
+          æScan. Aeternity Establishment has no
+          control over and assumes no responsibility for the content, privacy policies, or practices of any
+          third-party websites or resources.
+        </p>
+        <p class="terms-of-service__paragraph">
+          You acknowledge and agree that Aeternity Establishment is not responsible or liable for any damage
+          or loss caused or alleged to be
+          caused by or in connection with your use of or reliance on any such content, goods, or services
+          available on or through any such website
+          or resource.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Disclaimer of Warranties
+        </h2>
+        <p class="terms-of-service__paragraph">
+          æScan is provided on an "as is" and "as available" basis, without any warranties of any kind, either
+          express or implied. Aeternity
+          Establishment makes no representations or warranties of any kind, whether express or implied, as to
+          the operation of the site, the
+          information, content, materials, or products included on the site, or the results that may be
+          obtained from using the site.
+        </p>
+        <p class="terms-of-service__paragraph">
+          Aeternity Establishment does not warrant that æScan will be uninterrupted, timely, secure, or
+          error-free, nor does Aeternity Establishment
+          make any warranty as to the accuracy, completeness, or reliability of any information, content,
+          materials, or products included on the site.
+        </p>
+      </li>
+      <li>
+        <h2 class="terms-of-service__subheading">
+          Entire Agreement
+        </h2>
+        <p class="terms-of-service__paragraph">
+          These terms and conditions, together with the Privacy Policy, constitute the entire agreement
+          between you and Aeternity Establishment
+          with respect to your use of æScan, and supersede all prior or contemporaneous communications and
+          proposals, whether oral or written,
+          between you and Aeternity Establishment.
+        </p>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<style scoped>
+.terms-of-service {
+  max-width: 680px;
+  margin: 0 auto;
+
+  &__heading {
+    margin-bottom: var(--space-6);
+    font-size: 40px;
+    line-height: 48px;
+    letter-spacing: 0.03em;
+  }
+
+  &__subheading {
+    margin-bottom: var(--space-5);
+  }
+
+  &__paragraph {
+    margin-bottom: var(--space-5);
+    font-family: var(--font-monospaced);
+  }
+}
+</style>

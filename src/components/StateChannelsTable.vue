54951768ba38dda77e5c96ac6b8aa3ee17638ba0
janmichek <jan@janmichek.cz>
2023-05-11 09:29:17 +0200

    fix table markup

--- src/components/StateChannelsTable.vue
+++ src/components/StateChannelsTable.vue
@@ -1,51 +1,55 @@
 <template>
   <table>
-    <tr>
-      <th>State Channel ID</th>
-      <th>Status</th>
-      <th>Participants</th>
-      <th>On-Chain Updates</th>
-      <th>Locked</th>
-      <th>Last Known Round</th>
-      <th>Last Updated</th>
-      <th>Last TX type</th>
-    </tr>
-    <tr
-      v-for="channel in stateChannels.data"
-      :key="channel.id">
-      <td>
-        <value-hash-ellipsed
-          :hash="channel.id"
-          :link-to="`/state-channels/${channel.id}`"/>
-      </td>
-      <td>{{ channel.status }}</td>
-      <td>
-        <div>
-          Initiator:
+    <thead>
+      <tr>
+        <th>State Channel ID</th>
+        <th>Status</th>
+        <th>Participants</th>
+        <th>On-Chain Updates</th>
+        <th>Locked</th>
+        <th>Last Known Round</th>
+        <th>Last Updated</th>
+        <th>Last TX type</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="channel in stateChannels.data"
+        :key="channel.id">
+        <td>
           <value-hash-ellipsed
-            :hash="channel.initiator"
-            :link-to="`/accounts/${channel.initiator}`"/>
-        </div>
-        <div>
-          Responder:
-          <value-hash-ellipsed
-            :hash="channel.responder"
-            :link-to="`/accounts/${channel.responder}`"/>
-        </div>
-      </td>
-      <td>{{ channel.updateCount }}</td>
-      <td>{{ channel.locked }}</td>
-      <td>{{ channel.lastRound }}</td>
-      <td>
-        <div>
-          {{ channel.updatedHeight }}
-        </div>
-        <div>
-          <datetime-label :datetime="channel.updated "/>
-        </div>
-      </td>
-      <td>{{ channel.updateType }}</td>
-    </tr>
+            :hash="channel.id"
+            :link-to="`/state-channels/${channel.id}`"/>
+        </td>
+        <td>{{ channel.status }}</td>
+        <td>
+          <div>
+            Initiator:
+            <value-hash-ellipsed
+              :hash="channel.initiator"
+              :link-to="`/accounts/${channel.initiator}`"/>
+          </div>
+          <div>
+            Responder:
+            <value-hash-ellipsed
+              :hash="channel.responder"
+              :link-to="`/accounts/${channel.responder}`"/>
+          </div>
+        </td>
+        <td>{{ channel.updateCount }}</td>
+        <td>{{ channel.locked }}</td>
+        <td>{{ channel.lastRound }}</td>
+        <td>
+          <div>
+            {{ channel.updatedHeight }}
+          </div>
+          <div>
+            <datetime-label :datetime="channel.updated "/>
+          </div>
+        </td>
+        <td>{{ channel.updateType }}</td>
+      </tr>
+    </tbody>
   </table>
 </template>
 <script setup>

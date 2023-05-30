7f95ac98112185258b1829732284776788e20e66
janmichek <jan@janmichek.cz>
2023-05-30 12:50:36 +0200

    change text by proposals

--- src/utils/hints/oracles.js
+++ src/utils/hints/oracles.js
@@ -1,6 +1,6 @@
 export const oraclesHints = {
-  oracleId: 'Identifier of Oracle that is similar to the address of the Account that registered the Oracle. The only difference is the prefix that will be ok_ instead of ak_. This means that each account can only host 1 oracle. It\'s not possible to manage multiple oracles using the same account.',
-  oracleRegistered: 'To register an Oracle you need to provide a queryFormat and a responseFormat to the registerOracle function of the SDK. In addition to the common transaction options you can provide the oracle specific options queryFee and oracleTtlValue',
-  oracleExpiration: 'By default the Oracle will exist for the next 500 Keyblocks. If you intend to keep your oracle running longer you should increase the oracleTtlValue and/or set up a service that automatically extends the TTL before it expires.',
-  queryFee: 'Fee for querying Oracle. QueryFee is optional and defaults to 30000',
+  oracleId: 'Unique Oracle identifier inherited from the account address of the creator.',
+  oracleRegistered: 'Date, time, and block height when the Oracle has been registered via OracleRegisterTx.',
+  oracleExpiration: 'By default the Oracle will exist for the next 500 Keyblocks. To keep an Oracle running longer the oracleTtlValue should be increased and/or set up a service that automatically extends the TTL before it expires.',
+  queryFee: 'Fee for querying an Oracle. QueryFee is optional and defaults to 30000 aettos',
 }

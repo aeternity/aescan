c7bf0e6f3ec7b9caa4e9206bd5f6e0d1838f7271
janmichek <jan@janmichek.cz>
2023-05-30 16:53:30 +0200

    fix wording grammar

--- src/utils/hints/tokensHints.js
+++ src/utils/hints/tokensHints.js
@@ -1,5 +1,5 @@
 export const tokenHints = {
-  tokenSymbol: 'Symbol of a token. Symbol does not have to be unique.',
-  tokenName: 'Full name of a token.',
-  tokenContractId: 'Identifier of a Smart Contract that implements AEX-9 interface.',
+  tokenSymbol: 'Symbol of a token. The symbol does not have to be unique.',
+  tokenName: 'Full name of the token.',
+  tokenContractId: 'Identifier of a the Smart Contract that implements AEX-9 interface.',
 }

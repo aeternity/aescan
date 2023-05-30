7d2c48eabbdd4b73a27b48743b81d930b41d7404
janmichek <jan@janmichek.cz>
2023-05-30 12:36:59 +0200

    change text by proposals

--- src/utils/hints/contracts.js
+++ src/utils/hints/contracts.js
@@ -1,6 +1,6 @@
 export const contractsHints = {
-  contractId: 'A Smart Contract instance is an entity living on the block chain or in a state channel. Each instance has an address that can be used to call its entrypoints, either from another Smart Contract or in a ContractCallTx.',
-  contractCreated: 'Date, time and block height when the Smart Contract was created.',
-  contractTxHash: 'The transaction of type ContractCreateTx that created the Smart Contract on the blockchain.',
+  contractId: 'Unique identifier for a Smart Contract instance living on the blockchain',
+  contractCreated: 'Estimated date, time and block height when the Smart Contract was created.',
+  contractTxHash: 'The transaction hash of the ContractCreateTx that was executed to create the Smart Contract on the blockchain.',
   contractCreator: 'Account that created the Smart Contract.',
 }

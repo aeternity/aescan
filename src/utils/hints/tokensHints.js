dd5c2800aaff250171c79dc333adeaeb8c8fca70
janmichek <jan@janmichek.cz>
2023-05-31 11:01:09 +0200

    simplify text

--- src/utils/hints/tokensHints.js
+++ src/utils/hints/tokensHints.js
@@ -1,5 +1,5 @@
 export const tokenHints = {
-  tokenSymbol: 'Symbol of a token. The symbol does not have to be unique.',
+  tokenSymbol: 'Non-unique symbol of the token.',
   tokenName: 'Full name of the token.',
   tokenContractId: 'Identifier of a the Smart Contract that implements AEX-9 interface.',
 }

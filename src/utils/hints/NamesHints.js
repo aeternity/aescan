19dc7fa8f5742eb8e55aa9ce94b9a1c659a36c2e
janmichek <jan@janmichek.cz>
2023-06-01 09:18:46 +0200

    add part of hints

---
+++ src/utils/hints/NamesHints.js
@@ -0,0 +1,7 @@
+export const namesHints = {
+  name: 'Name that can be use instead of address.',
+  nameInAuction: 'Name that can be use instead of address. Names shorter than 13 characters needs to be auctioned',
+  expires: 'Estimated date, time, and block height when the name will be detached from the address.',
+  buyer: 'Address of the account which paid for the name.',
+  owner: 'Owner of the Name.',
+}

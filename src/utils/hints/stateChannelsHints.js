443e774305b7aacd4557130e28c2ca9cc397e287
janmichek <jan@janmichek.cz>
2023-06-05 10:58:33 +0200

    change hint content

--- src/utils/hints/stateChannelsHints.js
+++ src/utils/hints/stateChannelsHints.js
@@ -1,10 +1,10 @@
 export const stateChannelsHints = {
   stateChannelId: 'Unique identifier of the State Channel.',
-  status: 'State Channel can be Open or Closed. When status is Open the transactions lies outside of the blockchain network.',
+  status: 'When status is \'Open\' transactions can be executed inside the State Channel, outside the blockchain network',
   participants: 'Initiator is an account that created the State Channel via ChannelCreateTx. Responder is an account that interacted with the State Channel as a last one.',
-  onChainUpdates: 'Amount of an off-chain transactions within the State Channel.',
+  onChainUpdates: 'Amount of off-chain transactions within the State Channel.',
   locked: 'Currently deposited amount to the State Channel by it\'s participants.',
-  lastKnownRound: '',
-  lastUpdated: 'Estimated date, time, and block height of the last interaction within the State Channel.',
+  lastKnownRound: 'Operations in State Channels are executed in Rounds, where each action is one Round. The last known Round is the latest on-chain Round known publicly.',
+  lastUpdated: 'Estimated date, time, and block height of the last interaction with the State Channel.',
   lastTxType: 'Last transaction\'s type of the State Channel.',
 }

fd8128df682a0cdda89fa93d319d35cced4eee9b
janmichek <jan@janmichek.cz>
2023-06-06 10:51:36 +0200

    add more account hints

--- src/utils/hints/accountHints.js
+++ src/utils/hints/accountHints.js
@@ -4,4 +4,11 @@ export const accountHints = {
   transactions: 'Amount of transactions dispatched by account.',
   aensNames: 'Amount of Names owned by account.',
   nonce: 'Value of the nonce for the account. Nonce serves as a one-time authenticator for transaction.',
+  apiLinks: 'Link to detailed information about account.',
+  hash: 'Transaction hash of transaction initated by account.',
+  time: 'Estimated date, time and block height when thetransaction initiated by account.',
+  type: 'Type of the transaction initiated by account.',
+  name: 'Name owned by this account.',
+  expires: 'Estimated date, time and block height when the name owned by account expires.',
+  pointsTo: 'Account that the name owned by account points to.',
 }

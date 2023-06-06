d7a0a1a38f9a44aec4506d3c8e2cbec015cd2295
janmichek <jan@janmichek.cz>
2023-06-06 10:13:06 +0200

    add basic account hints

---
+++ src/utils/hints/accountHints.js
@@ -0,0 +1,7 @@
+export const accountHints = {
+  balance: 'Current amount of AE owned by account',
+  value: 'Current fiat value of AE owned by account',
+  transactions: 'Amount of transactions dispatched by account.',
+  aensNames: 'Amount of Names owned by account.',
+  nonce: 'Value of the nonce for the account. Nonce serves as a one-time authenticator for transaction.',
+}

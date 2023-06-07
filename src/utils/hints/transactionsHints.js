db5fe0f981b9a0a7b02d6a9ad2559ed25be8a47c
janmichek <jan@janmichek.cz>
2023-06-07 14:17:57 +0200

    add basic hints

---
+++ src/utils/hints/transactionsHints.js
@@ -0,0 +1,5 @@
+export const transactionsHints = {
+  hash: 'Unique identifier of the transaction.',
+  created: 'Estimated date, time, and block height when the transaction has been send.',
+  type: 'Every transaction in Aeternity ecosystem has its own type depending on its purpose',
+}

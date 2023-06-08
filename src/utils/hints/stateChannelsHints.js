273bc1ef28c0f6ef4d90486108e0c8a8ac1ea505
janmichek <jan@janmichek.cz>
2023-06-08 16:07:35 +0200

    fix wording after feedback

--- src/utils/hints/stateChannelsHints.js
+++ src/utils/hints/stateChannelsHints.js
@@ -1,7 +1,7 @@
 export const stateChannelsHints = {
   stateChannelId: 'Unique identifier of the State Channel.',
   status: 'When status is \'Open\' transactions can be executed inside the State Channel, outside the blockchain network',
-  participants: 'Initiator is an account that created the State Channel via ChannelCreateTx. Responder is an account that interacted with the State Channel as a last one.',
+  participants: 'Initiator is the account that created the State Channel via ChannelCreateTx.',
   onChainUpdates: 'Amount of off-chain transactions within the State Channel.',
   locked: 'Currently deposited amount to the State Channel by it\'s participants.',
   lastUpdated: 'Estimated date, time, and block height of the last interaction with the State Channel.',

6a698b6437b1f2f78620427c28785b9970e1bbcc
janmichek <jan@janmichek.cz>
2023-06-13 09:34:42 +0200

    fix wording

--- src/utils/hints/namesHints.js
+++ src/utils/hints/namesHints.js
@@ -1,7 +1,7 @@
 export const namesHints = {
   name: 'Unique identifier that may be configured to point to accounts, contracts or oracles.',
   nameInAuction: 'Unique identifier that may be configured to point to accounts, contracts or oracles. Names shorter than 13 characters need to be auctioned.',
-  expires: 'Estimated date, time, and block height when the name will be detached from the account.',
+  expires: 'Block height and estimated date and time when the name will be detached from the account.',
   buyer: 'Address of the account which paid for the Name and price of the Name.',
   owner: 'Owner of the Name.',
   pointsTo: 'Address that Name points to.',

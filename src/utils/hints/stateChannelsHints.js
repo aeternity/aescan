39de8d6e3613ff68089a298597cf37f80febad73
janmichek <jan@janmichek.cz>
2023-06-13 09:29:00 +0200

    fix wording

--- src/utils/hints/stateChannelsHints.js
+++ src/utils/hints/stateChannelsHints.js
@@ -4,6 +4,6 @@ export const stateChannelsHints = {
   participants: 'Initiator is the account that created the State Channel via ChannelCreateTx.',
   onChainUpdates: 'Amount of on-chain transactions within the State Channel.',
   locked: 'Currently deposited amount to the State Channel by its participants.',
-  lastUpdated: 'Estimated date, time, and block height of the last interaction with the State Channel.',
+  lastUpdated: 'Block height and estimated date and time of the last interaction with the State Channel.',
   lastTxType: 'Last transaction\'s type of the State Channel.',
 }

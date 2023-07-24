097104aa8463ec716a21a5521040d5aee20333ea
Łukasz Romanowicz <46789227+lukeromanowicz@users.noreply.github.com>
2023-07-24 09:51:47 +0200

    feat: keyblock and microblock links (#380)

--- src/components/ContractCallTransactionsTableCondensed.vue
+++ src/components/ContractCallTransactionsTableCondensed.vue
@@ -30,7 +30,10 @@
         </th>
         <td class="contract-call-transactions-table-condensed__data">
           <div>
-            {{ transaction.createdHeight }}
+            <app-link
+              :to="`/keyblocks/${transaction.createdHeight}`">
+              {{ transaction.createdHeight }}
+            </app-link>
           </div>
           <datetime-label :datetime="transaction.created"/>
         </td>
--- src/stores/keyblockDetails.js
+++ src/stores/keyblockDetails.js
@@ -10,18 +10,27 @@ export const useKeyblockDetailsStore = defineStore('keyblockDetails', () => {
   const keyblockDeltaStats = ref(null)
 
   const keyblockDetails = computed(() => {
-    return rawKeyblock.value ? adaptKeyblock(rawKeyblock.value, keyblockDeltaStats.value) : null
+    return rawKeyblock.value?.hash ? adaptKeyblock(rawKeyblock.value, keyblockDeltaStats.value) : rawKeyblock.value
   })
 
-  async function fetchKeyblock(keyblockHash) {
-    await fetchKeyblockDetails(keyblockHash)
+  async function fetchKeyblock(keyblockId) {
+    await fetchKeyblockDetails(keyblockId)
     await fetchKeyblockDeltaStats(rawKeyblock.value.height)
   }
 
-  async function fetchKeyblockDetails(keyblockHash) {
+  async function fetchKeyblockDetails(keyblockId) {
     rawKeyblock.value = null
-    const { data } = await axios.get(`${MIDDLEWARE_URL}/v2/key-blocks/${keyblockHash}`)
-    rawKeyblock.value = data
+    try {
+      const { data } = await axios.get(`${MIDDLEWARE_URL}/v2/key-blocks/${keyblockId}`)
+      rawKeyblock.value = data
+    } catch (error) {
+      if (error.response?.status === 404) {
+        rawKeyblock.value = { notExistent: true }
+        return
+      }
+
+      throw error
+    }
   }
 
   async function fetchKeyblockDeltaStats(keyblockHeight) {

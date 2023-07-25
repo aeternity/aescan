33f8bf97205972c1e7d72599b3569c7d0c04af8b
Jan Michek <15363559+janmichek@users.noreply.github.com>
2023-07-25 11:31:42 +0200

    feat: Microblock transactions table (#387)

---
+++ src/components/MicroblockTransactionsTableCondensed.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="microblock-transactions-table-condensed">
+    <table
+      v-for="transaction in transactions?.data"
+      :key="transaction.hash"
+      class="microblock-transactions-table-condensed__table">
+      <tbody>
+        <tr class="microblock-transactions-table-condensed__row">
+          <th class="microblock-transactions-table-condensed__header">
+            <app-tooltip>
+              Hash
+              <template #tooltip>
+                {{ microblocksHints.transactionHash }}
+              </template>
+            </app-tooltip>
+          </th>
+          <td class="microblock-transactions-table-condensed__data">
+            <value-hash-ellipsed
+              :hash="transaction.hash"
+              :link-to="`/transactions/${transaction.hash}`"/>
+          </td>
+        </tr>
+        <tr class="microblock-transactions-table-condensed__row">
+          <th class="microblock-transactions-table-condensed__header">
+            <app-tooltip>
+              Time
+              <template #tooltip>
+                {{ microblocksHints.transactionTime }}
+              </template>
+            </app-tooltip>
+          </th>
+          <td class="microblock-transactions-table-condensed__data">
+            <div>
+              <app-link
+                :to="`/keyblocks/${transaction.createdHeight}`">
+                {{ transaction.createdHeight }}
+              </app-link>
+            </div>
+            <datetime-label :datetime="transaction.created"/>
+          </td>
+        </tr>
+        <tr class="microblock-transactions-table-condensed__row">
+          <th class="microblock-transactions-table-condensed__header">
+            <app-tooltip>
+              Type
+              <template #tooltip>
+                {{ microblocksHints.transactionType }}
+              </template>
+            </app-tooltip>
+          </th>
+          <td class="microblock-transactions-table-condensed__data">
+            <app-tooltip>
+              {{ transaction.type }}
+              <template #tooltip>
+                {{ transactionsHints[transaction.hintKey] }}
+              </template>
+            </app-tooltip>
+          </td>
+        </tr>
+        <tr class="microblock-transactions-table-condensed__row">
+          <th class="microblock-transactions-table-condensed__header">
+            <app-tooltip>
+              Data
+              <template #tooltip>
+                {{ microblocksHints.transactionData }}
+              </template>
+            </app-tooltip>
+          </th>
+          <td class="microblock-transactions-table-condensed__data">
+            <transaction-cell
+              :transaction-type="transaction.type"
+              :transaction-data="transaction.data"/>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script setup>
+import TransactionCell from '@/components/TransactionCell'
+import ValueHashEllipsed from '@/components/ValueHashEllipsed'
+import AppTooltip from '@/components/AppTooltip'
+import DatetimeLabel from '@/components/DatetimeLabel'
+import { microblocksHints } from '@/utils/hints/microblocksHints'
+import { transactionsHints } from '@/utils/hints/transactionsHints'
+
+defineProps({
+  transactions: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+.microblock-transactions-table-condensed {
+  padding: var(--space-4) 0 0;
+
+  &__table {
+    margin-bottom: var(--space-6);
+
+    &:last-of-type {
+      margin-bottom: var(--space-1);
+    }
+
+    @media (--desktop) {
+      padding: var(--space-4);
+    }
+  }
+
+  &__header {
+    border-bottom: 1px solid var(--color-midnight-15);
+  }
+
+  &__row:last-of-type &__header {
+    border-bottom: 0;
+  }
+
+  &__data {
+    text-align: right;
+  }
+}
+</style>
---
+++ src/utils/hints/microblocksHints.js
@@ -0,0 +1,6 @@
+export const microblocksHints = {
+  transactionHash: 'Unique identifier of the transaction.',
+  transactionTime: 'Keyblock height and exact date and time when the transaction was executed by inclusion into a microblock.',
+  transactionType: 'The type of the transaction depends on its purpose in the æternity ecosystem.',
+  transactionData: 'The data displayed differs for each tx type.',
+}

9b70342708041668c97c452c534ad68cb4813c47
janmichek <jan@janmichek.cz>
2023-07-25 11:26:43 +0200

    fix wording

--- src/utils/hints/microblocksHints.js
+++ src/utils/hints/microblocksHints.js
@@ -1,6 +1,6 @@
 export const microblocksHints = {
   transactionHash: 'Unique identifier of the transaction.',
-  transactionTime: 'Date and time when the transaction was created.',
+  transactionTime: 'Keyblock height and exact date and time when the transaction was executed by inclusion into a microblock.',
   transactionType: 'The type of the transaction depends on its purpose in the æternity ecosystem.',
   transactionData: 'The data displayed differs for each tx type.',
 }

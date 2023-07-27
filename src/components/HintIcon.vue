2d25d3c9de7f27fd2df484687a5fd35064c4a5b0
Jan Michek <15363559+janmichek@users.noreply.github.com>
2023-07-27 11:43:47 +0200

    fix: Low contrast of entity hint icon (#403)

--- src/components/HintIcon.vue
+++ src/components/HintIcon.vue
@@ -2,10 +2,20 @@
   <app-icon
     name="question"
     :size="17"
-    class="hint-icon"/>
+    :class="['hint-icon', {'hint-icon--dark': variant === 'dark'}]"/>
 </template>
+
 <script setup>
 import AppIcon from '@/components/AppIcon'
+
+defineProps({
+  variant: {
+    type: String,
+    default: 'light',
+    validator: val =>
+      ['light', 'dark'].includes(val),
+  },
+})
 </script>
 
 <style scoped>
@@ -13,5 +23,9 @@ import AppIcon from '@/components/AppIcon'
   position: relative;
   margin-bottom: 3px;
   color: var(--color-midnight-25);
+
+  &--dark {
+    color: var(--color-midnight);
+  }
 }
 </style>

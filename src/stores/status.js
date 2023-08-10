359b8c0f43b499913e35f8fc2cbdc49267a3cc19
Łukasz Romanowicz <46789227+lukeromanowicz@users.noreply.github.com>
2023-08-10 11:44:15 +0200

    feat: display correct Node version (#440)

--- src/stores/status.js
+++ src/stores/status.js
@@ -3,14 +3,25 @@ import axios from 'axios'
 import { useRuntimeConfig } from 'nuxt/app'
 
 export const useStatus = defineStore('status', () => {
-  const { MIDDLEWARE_URL } = useRuntimeConfig().public
+  const { MIDDLEWARE_URL, NODE_URL } = useRuntimeConfig().public
   const middlewareVersion = ref(null)
   const nodeVersion = ref(null)
 
-  async function fetchStatus() {
+  async function fetchMdwStatus() {
     const { data } = await axios.get(`${MIDDLEWARE_URL}/v2/status`)
     middlewareVersion.value = data.mdwVersion
+  }
+
+  async function fetchNodeStatus() {
+    const { data } = await axios.get(`${NODE_URL}/v3/status`)
     nodeVersion.value = data.nodeVersion
+  }
+
+  function fetchStatus() {
+    return Promise.all([
+      fetchMdwStatus(),
+      fetchNodeStatus(),
+    ])
   }
 
   return {

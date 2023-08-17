02c1d14ee9c1de4deac4fd2c78d666df6e6604cd
janmichek <jan@janmichek.cz>
2023-08-17 14:27:30 +0200

    print search results to table

--- src/pages/search/[id].vue
+++ src/pages/search/[id].vue
@@ -1,30 +1,56 @@
 <template>
-  <div
+  <Head>
+    <Title>{{ APP_TITLE_SHORT }} | Search</Title>
+  </Head>
+  <page-header>
+    Search
+  </page-header>
+
+  <app-panel
     v-if="namesFound && tokensFound"
     class="">
-    <br>
-    <!--    names found: {{ namesFound }}-->
-    <table>
-      <tr v-for="name in namesFound.data">
-        <td>{{ name.payload.name }}</td>
-      </tr>
-    </table>
-
-    <br>
-    <br>
-    <br>
-    <!--    tokens found: {{ tokensFound }}-->
-    <table>
-      <tr v-for="token in tokensFound.data">
-        <td>{{ token.name }}</td>
-      </tr>
-    </table>
-  </div>
+    <app-tabs>
+      <app-tab title="Names">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Status</th>
+          </tr>
+          <tr
+            v-for="name in namesFound.data"
+            :key="name.payload.name">
+            <td>{{ name.payload.name }}</td>
+            <td>{{ parseStatus(name.payload) }}</td>
+          </tr>
+        </table>
+      </app-tab>
+      <app-tab title="Tokens">
+        <table>
+          <tr>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Smart Contract</th>
+          </tr>
+          <tr
+            v-for="token in tokensFound.data"
+            :key="token.contractId">
+            <td>{{ token.symbol }}</td>
+            <td>{{ token.name }}</td>
+            <td>{{ token.contractId }}</td>
+          </tr>
+        </table>
+      </app-tab>
+    </app-tabs>
+  </app-panel>
 </template>
 
 <script setup>
-import { useRoute, useRuntimeConfig } from '#app'
 import axios from 'axios'
+
+import { useRoute, useRuntimeConfig } from '#app'
+import PageHeader from '~/components/PageHeader'
+import AppTabs from '~/components/AppTabs'
+import AppTab from '~/components/AppTab'
 
 const route = useRoute()
 const { MIDDLEWARE_URL } = useRuntimeConfig().public
@@ -36,11 +62,21 @@ onMounted(() => {
 const namesFound = ref([])
 const tokensFound = ref([])
 
+function parseStatus(name) {
+  if (name.auctionEnd) {
+    return 'In Auction'
+  } else if (name.active) {
+    return 'Active'
+  } else {
+    return 'Expired'
+  }
+}
+
 async function searchByName(name) {
   // todo promise
   const { data: namesdata } = await axios.get(`${MIDDLEWARE_URL}/v2/names/search?prefix=${name}`)
   const { data: tokensdata } = await axios.get(`${MIDDLEWARE_URL}/v2/aex9?limit=10&prefix=${name}`)
-  console.log('namesdata', namesdata)
+  console.log('tokensdata', tokensdata)
   namesFound.value = namesdata
   tokensFound.value = tokensdata
 }

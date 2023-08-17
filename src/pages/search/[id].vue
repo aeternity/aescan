1989405038df19f86022621c9ff01b59e68b883b
janmichek <jan@janmichek.cz>
2023-08-17 15:47:30 +0200

    move logic to search store

--- src/pages/search/[id].vue
+++ src/pages/search/[id].vue
@@ -5,55 +5,77 @@
   <page-header>
     Search
   </page-header>
-
+  <br>
+  <br>
+  a{{ namesFound }}
+  <br>
+  <br>
+  b{{ tokensFound }}
+  <br><br>
   <app-tabs v-if="namesFound && tokensFound">
     <app-tab title="Names">
       <app-panel>
-        <table>
-          <tr>
-            <th>
-              Name
-            </th>
-            <th>Status</th>
-          </tr>
-          <tr
-            v-for="name in namesFound.data"
-            :key="name.payload.name">
-            <td>
-              <app-link :to="`/names/${name.payload.name}`">
-                {{ name.payload.name }}
-              </app-link>
-            </td>
-            <td>{{ parseStatus(name.payload) }}</td>
-          </tr>
-        </table>
+        <paginated-content
+          :entities="namesFound"
+          :limit="limit"
+          pagination-style="history"
+          @prev-clicked="loadPrevNamesFound"
+          @next-clicked="loadNextNamesFound">
+          <table>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>Status</th>
+            </tr>
+            <tr
+              v-for="name in namesFound.data"
+              :key="name.payload.name">
+              <td>
+                <app-link :to="`/names/${name.payload.name}`">
+                  {{ name.payload.name }}
+                </app-link>
+              </td>
+              <td>{{ parseStatus(name.payload) }}</td>
+            </tr>
+          </table>
+        </paginated-content>
       </app-panel>
     </app-tab>
     <app-tab title="Tokens">
       <app-panel>
-        <table>
-          <tr>
-            <th>Symbol</th>
-            <th>Name</th>
-            <th>Smart Contract</th>
-          </tr>
-          <tr
-            v-for="token in tokensFound.data"
-            :key="token.contractId">
-            <td>
-              <app-link
-                :to="`/tokens/${token.contractId}`">
-                <token-symbol-icon
-                  :contract-id="token.contractId"/>
-                {{ token.symbol }}
-              </app-link>
-            </td>
-            <td>{{ token.name }}</td>
-            <td>{{ token.contractId }}</td>
-            <!--            tokens icons-->
-            <!--            todo pagination-->
-          </tr>
-        </table>
+        <paginated-content
+          :entities="tokensFound"
+          :limit="limit"
+          pagination-style="history"
+          @prev-clicked="loadPrevTokensFound"
+          @next-clicked="loadNextTokensFound">
+          <table>
+            <tr>
+              <th>Symbol</th>
+              <th>Name</th>
+              <th>Smart Contract</th>
+            </tr>
+            <tr
+              v-for="token in tokensFound.data"
+              :key="token.contractId">
+              <td>
+                <app-link
+                  :to="`/tokens/${token.contractId}`">
+                  <token-symbol-icon
+                    :contract-id="token.contractId"/>
+                  {{ token.symbol }}
+                </app-link>
+              </td>
+              <td>{{ token.name }}</td>
+              <td>{{ token.contractId }}</td>
+              <!--            tokens icons-->
+              <!--            todo pagination-->
+              <!--            todo hints-->
+              <!--            loading-->
+            </tr>
+          </table>
+        </paginated-content>
       </app-panel>
     </app-tab>
   </app-tabs>
@@ -63,21 +85,25 @@
       Accounts
     </template>
   </app-panel>
+
   <app-panel>
     <template #heading>
       Transactions
     </template>
   </app-panel>
+
   <app-panel>
     <template #heading>
       Smart Contracts
     </template>
   </app-panel>
+
   <app-panel>
     <template #heading>
       Oracles
     </template>
   </app-panel>
+
   <app-panel>
     <template #heading>
       State Channels
@@ -86,24 +112,31 @@
 </template>
 
 <script setup>
-import axios from 'axios'
-
 import { useRoute, useRuntimeConfig } from '#app'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 import PageHeader from '~/components/PageHeader'
 import AppTabs from '~/components/AppTabs'
 import AppTab from '~/components/AppTab'
 import TokenSymbolIcon from '~/components/TokenSymbolIcon'
 import AppLink from '~/components/AppLink'
+import AppPanel from '~/components/AppPanel'
+import PaginatedContent from '~/components/PaginatedContent'
+import { isDesktop } from '~/utils/screen'
+import { useSearchStore } from '~/stores/search'
+
+const searchStore = useSearchStore()
+const { namesFound, tokensFound } = storeToRefs(searchStore)
+const { fetchTokenSearch, fetchNamesSearch } = searchStore
+
+const limit = computed(() => process.client && isDesktop() ? 10 : 3)
 
 const route = useRoute()
-const { MIDDLEWARE_URL } = useRuntimeConfig().public
 
 onMounted(() => {
-  searchByName(route.params.id)
+  fetchTokenSearch({ query: route.params.id, limit: limit.value })
+  fetchNamesSearch({ query: route.params.id, limit: limit.value })
 })
-
-const namesFound = ref([])
-const tokensFound = ref([])
 
 function parseStatus(name) {
   if (name.auctionEnd) {
@@ -115,12 +148,20 @@ function parseStatus(name) {
   }
 }
 
-async function searchByName(name) {
-  // todo promise
-  const { data: namesdata } = await axios.get(`${MIDDLEWARE_URL}/v2/names/search?prefix=${name}`)
-  const { data: tokensdata } = await axios.get(`${MIDDLEWARE_URL}/v2/aex9?limit=10&prefix=${name}`)
-  namesFound.value = namesdata
-  tokensFound.value = tokensdata
+async function loadPrevTokensFound() {
+  await fetchTokenSearch({ queryParameters: tokensFound.value.prev })
+}
+
+async function loadNextTokensFound() {
+  await fetchTokenSearch({ queryParameters: tokensFound.value.next })
+}
+
+async function loadPrevNamesFound() {
+  await fetchNamesSearch({ queryParameters: namesFound.value.prev })
+}
+
+async function loadNextNamesFound() {
+  await fetchNamesSearch({ queryParameters: namesFound.value.next })
 }
 
 </script>
---
+++ src/stores/search.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { useRuntimeConfig } from 'nuxt/app'
+import { defineStore } from 'pinia'
+
+export const useSearchStore = defineStore('search', () => {
+  const { MIDDLEWARE_URL } = useRuntimeConfig().public
+
+  const namesFound = ref([])
+  const tokensFound = ref([])
+
+  async function fetchNamesSearch({ query, limit, queryParameters } = {}) {
+    namesFound.value = null
+    const defaultParameters = `/v2/names/search?prefix=${query}&limit=${limit ?? 10}`
+    const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
+    namesFound.value = data
+  }
+
+  async function fetchTokenSearch({ query, limit, queryParameters } = {}) {
+    tokensFound.value = null
+    const defaultParameters = `/v2/aex9?prefix=${query}&limit=${limit ?? 10}`
+    const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
+    tokensFound.value = data
+  }
+
+  return {
+    namesFound,
+    tokensFound,
+    fetchTokenSearch,
+    fetchNamesSearch,
+  }
+})

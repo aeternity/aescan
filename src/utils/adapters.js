b4b2bb3dbbffcf31cac33bb535c9ff5cc8533881
janmichek <jan@janmichek.cz>
2023-08-28 14:29:28 +0200

    adapt data in adapter

--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -556,6 +556,38 @@ export function adaptStateChannels(channels, blockHeight) {
   }
 }
 
+export function adaptNftDetails(nft) {
+  function getTokenLimit(extensions, tokenLimit) {
+    if (extensions.includes('mintable') && extensions.includes('mintable_limit')) {
+      return tokenLimit
+    } else if (extensions.includes('mintable') && !extensions.includes('mintable_limit')) {
+      return 'Unlimited'
+    } else if (extensions.includes('mintable_templates') && extensions.includes('mintable_templates_limit')) {
+      return null
+    } else if (extensions.includes('mintable_templates') && !extensions.includes('mintable_templates_limit')) {
+      return null
+    }
+  }
+
+  function getTemplateLimit(extensions, templateLimit) {
+    if (extensions.includes('mintable') && extensions.includes('mintable_limit')) {
+      return null
+    } else if (extensions.includes('mintable') || !extensions.includes('mintable_limit')) {
+      return null
+    } else if (extensions.includes('mintable_templates') && extensions.includes('mintable_templates_limit')) {
+      return 'Unlimited'
+    } else if (extensions.includes('mintable_templates') || !extensions.includes('mintable_templates_limit')) {
+      return templateLimit
+    }
+  }
+
+  return {
+    ...nft,
+    tokenLimit: getTokenLimit(nft.extensions, nft.limits.tokenLimit),
+    templateLimit: getTemplateLimit(nft.extensions, nft.limits.templateLimit),
+  }
+}
+
 export function adaptNamesResults(names) {
   const formattedData = names.data
     .map(name => {

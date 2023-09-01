af5a50b980b321eb3898c81d8734726e54c732a4
Jan Michek <15363559+janmichek@users.noreply.github.com>
2023-09-01 09:59:43 +0200

    Update src/utils/hints/nftHints.js
    Co-authored-by: Michele F. <michele-franchi@users.noreply.github.com>

--- src/utils/hints/nftHints.js
+++ src/utils/hints/nftHints.js
@@ -1,6 +1,6 @@
 export const nftsHints = {
   nft: 'A non-fungible token (NFT) is a unit of data stored on a blockchain, that certifies a digital asset to be unique and therefore not interchangeable. NFTs can be used to represent items such as photos, videos, audio, and other types of digital files.',
-  collectionName: 'A name of NFTs collection of .',
+  collectionName: 'Name of the NFT collection.',
   owners: 'The number of owners of NFTs collection.',
   amount: 'The number of NFTs in the collection.',
   smartContractId: 'Identifier of the smart contract that implements the AEX-141 non-fungible token standard.',

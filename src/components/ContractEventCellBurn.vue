51e427683b6cbd1412927e3b45db3bb9cf9ee175
luekromanowicz <lromanowicz1@gmail.com>
2023-09-04 19:44:09 +0200

    token cells

---
+++ src/components/ContractEventCellBurn.vue
@@ -0,0 +1,31 @@
+<template>
+  <value-hash-ellipsed
+    :hash="contractDetails.id"
+    :link-to="`/contracts/${contractDetails.id}`"/>
+
+  <transaction-arrow-right-icon/>
+
+  <app-chip size="sm">
+    {{ formatAePrice(formatAettosToAe(eventData[1])) }}
+  </app-chip>
+</template>
+
+<script setup>
+import { formatAePrice, formatAettosToAe } from '@/utils/format'
+import AppChip from '@/components/AppChip'
+import TransactionArrowRightIcon from '@/components/TransactionArrowRightIcon'
+import ValueHashEllipsed from '@/components/ValueHashEllipsed'
+
+const props = defineProps({
+  contractDetails: {
+    required: true,
+    type: Object,
+  },
+  event: {
+    required: true,
+    type: Object,
+  },
+})
+
+const eventData = computed(() => props.event.data)
+</script>
---
+++ src/components/ContractEventCellTokenLimitDecrease.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ eventData[0] }}
+
+  <transaction-arrow-right-icon/>
+
+  {{ eventData[1] }}
+</template>
+
+<script setup>
+import TransactionArrowRightIcon from '@/components/TransactionArrowRightIcon'
+
+const props = defineProps({
+  contractDetails: {
+    required: true,
+    type: Object,
+  },
+  event: {
+    required: true,
+    type: Object,
+  },
+})
+
+const eventData = computed(() => props.event.data)
+</script>

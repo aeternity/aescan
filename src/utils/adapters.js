2b411d2ddd63722a0e9a899b68ad8f0aac3b3170
janmichek <jan@janmichek.cz>
2023-09-20 15:15:18 +0200

    remove LAEX identification from adapter

--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -457,17 +457,6 @@ export function adaptListedTokens(tokens) {
       }
     })
 
-  const isMainnet = useRuntimeConfig().public.NETWORK_NAME.toLowerCase() === 'mainnet'
-
-  if (isMainnet && !formattedData.some(token => token.contractId === LAEX_CONTRACT_ID)) {
-    formattedData.unshift({
-      contractId: LAEX_CONTRACT_ID,
-      name: 'LÆXON',
-      symbol: 'LAEX',
-      isAe: false,
-    })
-  }
-
   return {
     next: null,
     data: formattedData,

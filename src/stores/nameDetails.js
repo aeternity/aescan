bc493ab5849d6a43473695ae7efc00f7fd489113
janmichek <jan@janmichek.cz>
2023-10-10 14:05:21 +0200

    fix endpoint address

--- src/stores/nameDetails.js
+++ src/stores/nameDetails.js
@@ -33,8 +33,9 @@ export const useNameDetailsStore = defineStore('nameDetails', () => {
 
   async function fetchNameActions({ nameHash = null, queryParameters = null }) {
     rawNameActions.value = null
-    const defaultParameters = `/v2/accounts/${nameHash}/activities`
+    const defaultParameters = `/v2/names/${nameHash}/history`
     const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
+    console.log('data', data)
     rawNameActions.value = data
   }
 

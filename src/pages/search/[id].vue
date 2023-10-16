9dbd8d70e9f74421d5eb8c77b6134eb1c3045379
janmichek <jan@janmichek.cz>
2023-10-16 14:41:43 +0200

    add ability to navigate tabs from browser

--- src/pages/search/[id].vue
+++ src/pages/search/[id].vue
@@ -7,7 +7,7 @@
   </page-header>
 
   <template v-if="!isLoading">
-    <app-tabs>
+    <app-tabs v-model="activeTabIndex">
       <app-tab title="Names">
         <search-names-panel/>
       </app-tab>
@@ -23,6 +23,37 @@
 import PageHeader from '@/components/PageHeader'
 import AppTabs from '@/components/AppTabs'
 import AppTab from '@/components/AppTab'
+
+const route = useRoute()
+const { push, replace } = useRouter()
+
+const TAB_KEYS = ['names', 'tokens']
+
+const activeTabIndex = computed({
+  get() {
+    const { type: activeTabName } = route.query
+
+    if (activeTabName === undefined) {
+      return 0
+    }
+
+    return TAB_KEYS.indexOf(activeTabName)
+  },
+  set(index) {
+    const newRoute = {
+      query: {
+        type: TAB_KEYS[index],
+      },
+    }
+
+    if (activeTabIndex.value === index) {
+      // if navigating back
+      return replace(newRoute)
+    }
+
+    return push(newRoute)
+  },
+})
 
 const { isLoading } = useLoading()
 </script>

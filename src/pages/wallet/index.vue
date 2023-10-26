5f7042a3155b4cccc1bdf0a30b2e28d9133c7bc9
janmichek <jan@janmichek.cz>
2023-10-26 14:18:51 +0200

    add basic connection UI

--- src/pages/wallet/index.vue
+++ src/pages/wallet/index.vue
@@ -1,14 +1,49 @@
 <template>
+  <Head>
+    <Title>Wallet Account</Title>
+  </Head>
+
+  <page-header>
+    Wallet Account
+    <template #tooltip>
+      <!--      todo hint-->
+      {{ transactionsHints.transaction }}
+    </template>
+  </page-header>
+
   <app-panel class="">
-    <div v-if="balance">
+    <template #heading>
+      {{ status === 'not installed' ? 'Wallet not found' : '' }}
+      {{ status === 'found' ? 'Connect Superhero wallet' : '' }}
+    </template>
+
+    <loader-indicator
+      v-if="status === 'scanning'"
+      label="Scanning for wallets"/>
+
+    <p
+      v-if="status === 'not installed'"
+      class="unexpected-error__paragraph">
+      In order to display wallet account information, the native Superhero wallet has to be installed on this device.
+    </p>
+
+    <div class="unexpected-error__container">
+      <app-button
+        v-if="status === 'not installed'"
+        to="https://wallet.superhero.com/">
+        Download Superhero Wallet
+      </app-button>
+      <div v-if="status === 'found'">
+        <wallet-connect-button/>
+      </div>
+    </div>
+
+    <div v-if="status === 'connected'">
       <br>
       {{ walletInfo }}
       <br>
       {{ balance }}
       <br>
-    </div>
-    <div v-else>
-      <wallet-connect-button/>
     </div>
   </app-panel>
 </template>
@@ -16,11 +51,20 @@
 <script setup>
 import { storeToRefs } from 'pinia'
 import { useWalletStore } from '~/stores/wallet'
+import { transactionsHints } from '~/utils/hints/transactionsHints'
+import PageHeader from '~/components/PageHeader'
+import AppButton from '~/components/AppButton'
 
 const walletStore = useWalletStore()
+const { scanWallets } = walletStore
 
 const {
   walletInfo,
   balance,
+  status,
 } = storeToRefs(walletStore)
+
+onMounted(async() => {
+  await scanWallets()
+})
 </script>

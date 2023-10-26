6a772ac45e8981b8c86963b365a7120b704be4c9
janmichek <jan@janmichek.cz>
2023-10-26 15:09:42 +0200

    componentize UI

---
+++ src/components/WalletAccountPanel.vue
@@ -0,0 +1,20 @@
+<template>
+  <app-panel>
+    <br>
+    {{ walletInfo }}
+    <br>
+    {{ balance }}
+    <br>
+  </app-panel>
+</template>
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useWalletStore } from '~/stores/wallet'
+
+const walletStore = useWalletStore()
+
+const {
+  walletInfo,
+  balance,
+} = storeToRefs(walletStore)
+</script>

1c75b479955a11d33541a69567ab9f69f89a4100
janmichek <jan@janmichek.cz>
2023-10-31 16:27:08 +0100

    display all account information

--- src/components/WalletAccountPanel.vue
+++ src/components/WalletAccountPanel.vue
@@ -1,19 +1,90 @@
 <template>
   <account-details-panel
     v-if="accountDetails"
-    class="account__account-details-panel"
+    class="wallet-account-panel__account-details-panel"
     :account-details="accountDetails"/>
+
+  <client-only>
+    <app-tabs
+      v-if="isTabsVisible"
+      v-model="activeTabIndex">
+      <app-tab title="Activities">
+        <account-activities-panel/>
+      </app-tab>
+      <app-tab title="Transactions">
+        <account-transactions-panel/>
+      </app-tab>
+      <app-tab title="AENS Names">
+        <account-names-panel/>
+      </app-tab>
+      <app-tab
+        title="Tokens"
+        :is-preselected="isTokensTabSelected">
+        <account-tokens-panel/>
+      </app-tab>
+    </app-tabs>
+  </client-only>
 </template>
 <script setup>
 import { storeToRefs } from 'pinia'
+import { useRoute, useRouter } from 'nuxt/app'
 import { useWalletStore } from '~/stores/wallet'
 import { useAccountStore } from '@/stores/accountDetails'
 
 const walletStore = useWalletStore()
 const accountStore = useAccountStore()
 const { aeSdk } = storeToRefs(walletStore)
-const { accountDetails } = storeToRefs(accountStore)
+const { accountDetails, accountTokens } = storeToRefs(accountStore)
 const { fetchAccount } = accountStore
+
+const TAB_KEYS = ['activities', 'transactions', 'aens-names', 'tokens']
+
+const { push, replace } = useRouter()
+
+const route = useRoute()
+
+const isTabsVisible = computed(() => process.client &&
+    ((accountDetails.value && !accountDetails.value?.notExistent) ||
+        !!accountTokens.value?.data.length))
+
+const isTokensTabSelected = computed(() => process.client &&
+    accountDetails.value?.notExistent &&
+    !!accountTokens.value?.data.length)
+
+const activeTabIndex = computed({
+  get() {
+    const { type: activeTabName } = route.query
+
+    if (activeTabName === undefined) {
+      return 0
+    }
+
+    return TAB_KEYS.indexOf(activeTabName)
+  },
+  set(index) {
+    const newRoute = {
+      query: {
+        type: TAB_KEYS[index],
+      },
+    }
+
+    if (activeTabIndex.value === index) {
+      // if navigating back
+      return replace(newRoute)
+    }
+
+    return push(newRoute)
+  },
+})
 
 fetchAccount(aeSdk.value.address)
 </script>
+
+<style scoped>
+.wallet-account-panel__account-details-panel {
+  margin-bottom: var(--space-4);
+  @media (--desktop) {
+    margin-bottom: var(--space-6);
+  }
+}
+</style>

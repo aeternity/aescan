9e6ac4ff0ae7d06ffcdb77a3dd295c10b07dd0e1
janmichek <jan@janmichek.cz>
2023-11-02 12:01:50 +0100

    fix connection controls

--- src/components/AppDropdown.vue
+++ src/components/AppDropdown.vue
@@ -10,16 +10,10 @@
   </VMenu>
 </template>
 
-<script setup>
-
-</script>
-
 <style>
 .v-popper__popper.v-popper--theme-menu .v-popper__inner {
   margin: 0 var(--space-2);
   color: var(--color-midnight);
   padding: var(--space-2) var(--space-1);
 }
-}
-
 </style>

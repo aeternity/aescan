f74ff96c476521109be60bdc5889ed841c0440f6
janmichek <jan@janmichek.cz>
2023-11-13 13:56:04 +0100

    fix cors attempt

--- src/server/plugins/response-headers.js
+++ src/server/plugins/response-headers.js
@@ -4,7 +4,7 @@ const DEFAULT_HEADERS = {
   'X-XSS-Protection': '1; mode=block',
   'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
   'Cache-control': 'no-cache',
-  'Content-Security-Policy': 'default-src \'self\' *; font-src \'self\' data:; img-src * data:; script-src \'self\' \'unsafe-inline\'; style-src \'self\' \'unsafe-inline\'; frame-src \'self\'',
+  'Content-Security-Policy': 'default-src \'self\' *; font-src \'self\' data:; img-src https://avatars.z52da5wt.xyz/ data:; script-src \'self\' \'unsafe-inline\'; style-src \'self\' \'unsafe-inline\'; frame-src \'self\'; upgrade-insecure-requests; block-all-mixed-content',
   'Permissions-Policy': 'camera=(), geolocation=(), microphone=()',
   'Referrer-Policy': 'strict-origin-when-cross-origin',
   'X-Permitted-Cross-Domain-Policies': 'none',

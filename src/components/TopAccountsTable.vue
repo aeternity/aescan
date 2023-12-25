8dccdd41f7e0437de0483fb5684ad74b145d9c91
janmichek <jan@janmichek.cz>
2023-12-25 11:29:01 +0100

    componentize UI

---
+++ src/components/TopAccountsTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <table class="top-accounts-table">
+    <tr>
+      <th>
+        Rank
+        <hint-tooltip>
+          {{ stateChannelsHints.stateChannelId }}
+        </hint-tooltip>
+      </th>
+      <th>
+        Account
+        <hint-tooltip>
+          {{ stateChannelsHints.stateChannelId }}
+        </hint-tooltip>
+      </th>
+      <th>
+        % Of Circulating
+        <hint-tooltip>
+          {{ stateChannelsHints.stateChannelId }}
+        </hint-tooltip>
+      </th>
+    </tr>
+    <tr
+      v-for="account in topAccounts"
+      :key="account.account">
+      <td>{{ account.rank }}.</td>
+      <td>
+        <app-link
+          :to="`/accounts/${account.account}`">
+          {{ account.account }}
+        </app-link>
+      </td>
+      <td>{{ account.percentage }} %</td>
+    </tr>
+  </table>
+</template>
+<script setup>
+import { stateChannelsHints } from '~/utils/hints/stateChannelsHints'
+
+defineProps({
+  topAccounts: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+.top-accounts-table {
+  margin-bottom: var(--space-4);
+}
+</style>
--- src/stores/topAccounts.js
+++ src/stores/topAccounts.js
@@ -4,14 +4,13 @@ export const useTopAccountsStore = defineStore('topAccounts', () => {
   const axios = useAxios()
   const rawTopAccounts = ref(null)
   const { MIDDLEWARE_URL } = useRuntimeConfig().public
-
   const blockchainStatsStore = useBlockchainStatsStore()
 
-  async function fetchTopAccounts() {
-    rawTopAccounts.value = null
-    const { data } = await axios.get(`${MIDDLEWARE_URL}/v2/wealth`)
-    rawTopAccounts.value = data
-  }
+  const topAccounts = computed(() =>
+    rawTopAccounts.value && distribution.value
+      ? adaptTopAccounts(rawTopAccounts.value, distribution.value)
+      : null,
+  )
 
   const distribution = computed(() =>
     blockchainStatsStore.totalTokenSupply && blockchainStatsStore.burnedCount
@@ -19,20 +18,14 @@ export const useTopAccountsStore = defineStore('topAccounts', () => {
       : null,
   )
 
-  const topAccounts = computed(() => {
-    // console.log('1 blockchainStatsStore', blockchainStatsStore)
-    // console.log('1 blockchainStatsStore.totalTokenSupply', blockchainStatsStore.totalTokenSupply.value)
-    // console.log('1 blockchainStatsStore.burnedCount', blockchainStatsStore.burnedCount.value)
-
-    return rawTopAccounts.value && distribution.value
-      ? adaptTopAccounts(rawTopAccounts.value, distribution.value)
-      : null
-  })
+  async function fetchTopAccounts() {
+    rawTopAccounts.value = null
+    const { data } = await axios.get(`${MIDDLEWARE_URL}/v2/wealth`)
+    rawTopAccounts.value = data
+  }
 
   return {
     topAccounts,
     fetchTopAccounts,
-    distribution,
   }
-  // todo remove dis
 })

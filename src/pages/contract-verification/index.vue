99c73de7e2042a1dd3059a95db1abf5c76b0e206
janmichek <jan@janmichek.cz>
2023-12-26 16:55:51 +0100

    add file upload UI

---
+++ src/pages/contract-verification/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Head>
+    <Title>Smart Contract Verification</Title>
+  </Head>
+
+  <page-header>
+    Smart Contract Verification
+    <template #tooltip>
+      {{ contractsHints.contract }}
+      <app-link
+        variant="primary"
+        to="https://docs.aeternity.com/protocol/contracts/">
+        Learn more
+      </app-link>
+    </template>
+  </page-header>
+  <app-panel>
+    <file-upload/>
+  </app-panel>
+</template>
+
+<script/>

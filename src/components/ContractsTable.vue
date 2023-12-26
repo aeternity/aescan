b0a3b021eca6ccbeef036cf9f1047cd2c2f84983
janmichek <jan@janmichek.cz>
2023-12-26 11:53:49 +0100

    add mocked UI

--- src/components/ContractsTable.vue
+++ src/components/ContractsTable.vue
@@ -26,6 +26,7 @@
             {{ contractsHints.creator }}
           </hint-tooltip>
         </th>
+        <th>Verified</th>
       </tr>
     </thead>
     <tbody>
@@ -55,6 +56,12 @@
           <value-hash-ellipsed
             :link-to="`/accounts/${contract.createdBy}`"
             :hash="contract.createdBy"/>
+        </td>
+        <td>
+          <app-icon
+            size="24"
+            class="contracts-table__icon"
+            name="verified"/>
         </td>
       </tr>
     </tbody>
@@ -86,6 +93,10 @@ defineProps({
     display: flex;
     align-items: center;
     gap: var(--space-0)
+  }
+
+  &__icon {
+    color: var(--color-success);
   }
 }
 </style>

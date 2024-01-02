29416208b61b93751226c6bdb7f0615776595336
janmichek <jan@janmichek.cz>
2024-01-02 15:28:05 +0100

    move computation to adapter

--- src/utils/adapters.js
+++ src/utils/adapters.js
@@ -8,6 +8,7 @@ import {
   formatDecodeBase64,
   formatIsAuction,
   formatNameState,
+  formatNumber,
   formatTemplateLimit,
   formatTokenLimit,
 } from '@/utils/format'
@@ -435,14 +436,20 @@ export function adaptTokenEvents(events) {
 }
 
 export function adaptTokenHolders(tokenHolders, tokenDetails) {
-  const formattedData = tokenHolders.data.map(holder => ({
-    address: holder.accountId,
-    contractId: holder.contractId,
-    amount: (new BigNumber(holder.amount)).dividedBy(10 ** tokenDetails.decimals).toNumber(),
-    percentage: (new BigNumber(holder.amount)
-      .dividedBy(10 ** (tokenDetails.decimals - 2)))
-      .dividedBy(tokenDetails.totalSupply).toNumber(),
-  }))
+  const formattedData = tokenHolders.data
+    .map(holder => {
+      const percentage = (new BigNumber(holder.amount)
+        .dividedBy(10 ** (tokenDetails.decimals - 2)))
+        .dividedBy(tokenDetails.totalSupply).toNumber()
+      return {
+        address: holder.accountId,
+        contractId: holder.contractId,
+        amount: (new BigNumber(holder.amount)).dividedBy(10 ** tokenDetails.decimals).toNumber(),
+        percentage: Math.abs(percentage) >= 0.00001
+          ? `${formatNumber(Math.abs(percentage))} %`
+          : '~0',
+      }
+    })
 
   return {
     next: tokenHolders.next,
@@ -557,7 +564,7 @@ export function adaptStateChannels(stateChannels) {
         initiator: channel.initiator,
         responder: channel.responder,
         updateCount: channel.updatesCount,
-        locked: formatAePrice(formatAettosToAe(channel.amount)),
+        locked: formatAettosToAe(channel.amount),
         updatedHeight: channel.lastUpdatedHeight,
         updated: DateTime.fromMillis(channel.lastUpdatedTime),
         lastTxType: channel.lastUpdatedTxType,

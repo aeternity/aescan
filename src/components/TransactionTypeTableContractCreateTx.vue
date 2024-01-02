c5fe7e0142fc6068f42b2709f0b4210b53b8bfe4
janmichek <jan@janmichek.cz>
2024-01-02 14:11:09 +0100

    fix alignment

--- src/components/TransactionTypeTableContractCreateTx.vue
+++ src/components/TransactionTypeTableContractCreateTx.vue
@@ -76,11 +76,13 @@
           Gas Price
         </th>
         <td class="transaction-type-panel-contract-create-tx__data">
-          <price-label :price="transactionData.gas"/>
-          /
-          <price-label
-            :price="formatAettosToAe(transactionData.gasPrice)"
-            :max-digits="null"/>
+          <div class="transaction-type-panel-contract-create-tx__container">
+            <price-label :price="transactionData.gas"/>
+            /
+            <price-label
+              :price="formatAettosToAe(transactionData.gasPrice)"
+              :max-digits="null"/>
+          </div>
         </td>
       </tr>
       <tr class="transaction-type-panel-contract-create-tx__row">
@@ -98,11 +100,13 @@
           Gas Costs
         </th>
         <td class="transaction-type-panel-contract-create-tx__data">
-          <price-label :price="transactionData.gasUsed"/>
-          /
-          <price-label
-            :price="formatAettosToAe(gasCosts)"
-            :max-digits="null"/>
+          <div class="transaction-type-panel-contract-create-tx__container">
+            <price-label :price="transactionData.gasUsed"/>
+            /
+            <price-label
+              :price="formatAettosToAe(gasCosts)"
+              :max-digits="null"/>
+          </div>
         </td>
       </tr>
     </tbody>
@@ -147,6 +151,12 @@ const gasCosts = computed(() =>
 
   &__row:last-of-type &__table-header {
     border-bottom: 0;
+  }
+
+  &__container {
+    display: inline-flex;
+    flex-direction: row;
+    gap: var(--space-0);
   }
 }
 </style>

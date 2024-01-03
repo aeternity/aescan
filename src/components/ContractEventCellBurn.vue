10c8d876bb6747fb0b7293df0eddfd4e8c7616e2
janmichek <jan@janmichek.cz>
2024-01-03 09:51:11 +0100

    fix contract event formatting

--- src/components/ContractEventCellBurn.vue
+++ src/components/ContractEventCellBurn.vue
@@ -5,14 +5,14 @@
 
   <transaction-arrow-right-icon/>
 
-  <app-chip size="sm">
-    {{ tokenValue }}
-  </app-chip>
+  <price-label
+    :price="formatAettosToAe(tokenValue)"
+    :contract-id="contractDetails.tokenDetails.contractId"
+    :currency="contractDetails.symbol"/>
 </template>
 
 <script setup>
 import { formatNumber, formatReduceDecimals } from '@/utils/format'
-import AppChip from '@/components/AppChip'
 import TransactionArrowRightIcon from '@/components/TransactionArrowRightIcon'
 import ValueHashEllipsed from '@/components/ValueHashEllipsed'
 

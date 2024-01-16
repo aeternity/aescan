11b0a1f7e98c047e9dd76b330c4f9aa61073e1f2
janmichek <jan@janmichek.cz>
2024-01-16 11:46:05 +0100

    print and format contract code

--- src/components/VerifiedContractPanel.vue
+++ src/components/VerifiedContractPanel.vue
@@ -1,10 +1,9 @@
 <template>
-  <app-panel>
+  <app-panel v-if="isVerified">
     <h3>Verification details</h3>
-    isVerified {{ isVerified }}
-    <hr>
-    verificationDetails {{ verificationDetails }}
-    <verified-contract-table/>
+    <verified-contract-table
+      :verification-details="verificationDetails"
+      :is-verified="isVerified"/>
     <header class="verification-contract-panel__header">
       <h3>Contract Code</h3>
       <div>
@@ -16,24 +15,29 @@
         </app-button>
       </div>
     </header>
-    <verified-contract-editor/>
+    <verified-contract-editor :verification-details="verificationDetails"/>
   </app-panel>
+  <div v-else>
+    Not verified: HINT
+  </div>
 </template>
+
 <script setup>
-import VerifiedContractTable from '~/components/VerifiedContractTable'
-import { useContractVerifiedStore } from '~/stores/contractVerified'
-
-const route = useRoute()
-
-const contractVerifiedStore = useContractVerifiedStore()
-const { isVerified, verificationDetails } = storeToRefs(contractVerifiedStore)
-const { fetchIsContractVerified } = contractVerifiedStore
-
-await fetchIsContractVerified(route.params.id)
+defineProps({
+  verificationDetails: {
+    required: true,
+    type: Object,
+  },
+  isVerified: {
+    required: true,
+    type: Object,
+  },
+})
 
 </script>
 
 <style scoped>
+/*//todo fix naming*/
 .verification-contract-panel {
   &__header {
     display: flex;

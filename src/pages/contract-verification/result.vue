87a7ce4d7b769e12b09c7ae3542a787f0db0075d
janmichek <jan@janmichek.cz>
2024-01-24 05:16:17 +0100

    improve result page UI and styles

--- src/pages/contract-verification/result.vue
+++ src/pages/contract-verification/result.vue
@@ -1,8 +1,9 @@
 <template>
   <Head>
-    <Title>Smart Contract Verification</Title>
+    <Title>Smart Contract Verification Result</Title>
   </Head>
   <page-header>
+    {{ title }}
     <template #tooltip>
       <!--            {{ contractsHints.contract }}-->
       <!--      todo hint-->
@@ -19,24 +20,25 @@
       label="Verifying"/>
 
     <div v-if="status === 'success'">
-      <h2>Smart Contract Verified</h2>
-      <p>
+      <p class="contract-verification-result__paragraph">
         Success! Your smart contract has been verified. See
         <app-link :to="`/contracts/${id}`">
           Smart Contract detail
         </app-link>
       </p>
+
+      <app-button to="/contract-verification">
+        Verify another Smart Contract
+      </app-button>
     </div>
+
     <div v-if="status === 'fail' || status === 'Conflict'">
-      <h2>Contract Verification Failed</h2>
-      <p>
-        <span class="error">
-          {{ message }}
-        </span>
-        <app-button to="/contract-verification">
-          Retry verification
-        </app-button>
+      <p class="contract-verification-result__paragraph">
+        {{ message }}
       </p>
+      <app-button to="/contract-verification">
+        Retry verification
+      </app-button>
     </div>
   </app-panel>
 </template>
@@ -51,14 +53,16 @@ const verificationStore = useContractVerificationStore()
 const { fetchVerificationStatus } = verificationStore
 const { result, id, verificationStatus } = storeToRefs(verificationStore)
 
-// fetchVerificationStatus(id.value, result.value.data.submissionId)
 // todo move to store
-// todo navigate out
 onMounted(() => {
   if (result.value.statusText !== 'Conflict') {
     loadVerificationStatus()
     timer.value = setInterval(loadVerificationStatus, 5000)
   }
+})
+
+onBeforeUnmount(() => {
+  clearInterval(timer.value)
 })
 
 const status = computed(() => {
@@ -73,6 +77,16 @@ const loadVerificationStatus = async() => {
   await fetchVerificationStatus(id.value, result.value.data.submissionId)
 }
 
+const title = computed(() => {
+  if (status.value === 'success') {
+    return 'Smart Contract Verified'
+  }
+  if (status.value === 'fail' || status.value === 'Conflict') {
+    return 'Contract Verification Failed'
+  }
+  return 'Smart Contract Verification Result'
+})
+
 watch(verificationStatus, newStatus => {
   if (newStatus.data.status === 'fail' || newStatus.data.status === 'success') {
     clearInterval(timer.value)
@@ -80,3 +94,11 @@ watch(verificationStatus, newStatus => {
   }
 })
 </script>
+
+<style scoped>
+.contract-verification-result {
+  &__paragraph {
+    margin-bottom: var(--space-3);
+  }
+}
+</style>

9ad1863fccc12430c5afeac55d5e109171d08ace
janmichek <jan@janmichek.cz>
2024-01-30 11:40:52 +0100

    fix is verified condition

--- src/components/VerifiedContractPanel.vue
+++ src/components/VerifiedContractPanel.vue
@@ -1,29 +1,34 @@
 <template>
-  <app-panel v-if="isVerified">
-    <h3>Verification details</h3>
-    <verified-contract-table
-      :verification-details="verificationDetails"
-      :is-verified="isVerified"/>
-    <header class="verification-contract-panel__header">
-      <h3>Contract Code</h3>
-      <div>
-        <app-button variant="light">
-          Export
-        </app-button>
-      </div>
-    </header>
-    <verified-contract-editor
-      :code="contractCode.source[0].content"
-      lang="aes"/>
-    <h3>ACI</h3>
-    <verified-contract-editor
-      :code="verificationDetails.aci"
-      lang="json"/>
+  <app-panel>
+    <template v-if="isVerified">
+      <h3>Verification details</h3>
+      <verified-contract-table
+        :verification-details="verificationDetails"
+        :is-verified="isVerified"/>
+      <header class="verification-contract-panel__header">
+        <h3>Contract Code</h3>
+        <div>
+          <app-button variant="light">
+            Export
+          </app-button>
+        </div>
+      </header>
+      <verified-contract-editor
+        :code="contractCode.source[0].content"
+        lang="aes"/>
+      <h3>ACI</h3>
+      <verified-contract-editor
+        :code="verificationDetails.aci"
+        lang="json"/>
+    </template>
+    <blank-state v-else>
+      Smart Contract is not verified. Go to
+      <app-link to="/contract-verification">
+        Contract Verification
+      </app-link>
+      to verify it.
+    </blank-state>
   </app-panel>
-  <div v-else>
-    Not verified: HINT
-  </div>
-
   <!--  todo print loop-->
 </template>
 
@@ -33,21 +38,12 @@ import { storeToRefs } from 'pinia'
 import { useContractVerifiedStore } from '~/stores/verified'
 
 const contractVerifiedStore = useContractVerifiedStore()
-const { contractCode } = storeToRefs(contractVerifiedStore)
+const { contractCode, verificationDetails, isVerified } = storeToRefs(contractVerifiedStore)
 const { fetchContractCode } = contractVerifiedStore
 const route = useRoute()
-await fetchContractCode(route.params.id)
-
-defineProps({
-  verificationDetails: {
-    required: true,
-    type: Object,
-  },
-  isVerified: {
-    required: true,
-    type: Object,
-  },
-})
+if (isVerified.value) {
+  await fetchContractCode(route.params.id)
+}
 
 </script>
 

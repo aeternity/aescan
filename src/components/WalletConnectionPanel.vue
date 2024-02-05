c61775da3de6b5b917322fb886955b379366cf5d
janmichek <jan@janmichek.cz>
2024-02-05 03:56:31 +0100

    fix code style

--- src/components/WalletConnectionPanel.vue
+++ src/components/WalletConnectionPanel.vue
@@ -71,6 +71,7 @@ const title = computed(() => {
 .wallet-connection-panel {
   &__paragraph {
     margin-bottom: var(--space-5);
+
     @media (--desktop) {
       &:last-of-type {
         margin-bottom: var(--space-6);

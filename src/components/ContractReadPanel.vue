8b7918297ee4b9d27368d19540c2a5c89935c602
janmichek <jan@janmichek.cz>
2024-02-07 08:37:50 +0100

    create write contract section

--- src/components/ContractReadPanel.vue
+++ src/components/ContractReadPanel.vue
@@ -3,9 +3,7 @@
     <h3 class="contract-read-panel__title">
       Read Smart Contract Information
     </h3>
-    <app-button>
-      Connect Wallet
-    </app-button>
+
     <div
       v-for="(aciFunction, index) in aciFunctions"
       :key="index"
@@ -23,17 +21,84 @@
         <p>
           {{ aciFunction.returns }}
         </p>
-        <app-button @click="fetchFunctionResponse()">
-          {{ aciFunction.stateful ? 'Call Locally' : 'Send Transaction' }}
-        </app-button>
+        <hr>
+        <form @submit.prevent="fetchFunctionResponse(aciFunction.name, index)">
+          <input
+            v-for="argument in aciFunction.arguments"
+            :id="aciFunction.name + '-' + argument.name"
+            v-model="form[aciFunction.name + '-' + argument.name]"
+            :name="aciFunction.name + '-' + argument.name"
+            :placeholder="argument.type"
+            type="text">
+          <button
+            type="submit">
+            {{ aciFunction.stateful ? 'Call Locally' : 'Send Transaction' }}
+          </button>
+        </form>
+        <hr>
+        <span v-if="response[index]">
+          Response {{ response }}
+        </span>
+      </div>
+    </div>
+    <h3 class="contract-read-panel__title">
+      Write Smart Contract Information
+    </h3>
+    <app-button>
+      Connect Wallet
+    </app-button>
+    <div
+      v-for="(aciFunction, index) in aciStatefulFunctions"
+      :key="index"
+      class="contract-read-panel__accordion">
+      <header
+        :class="[
+          'contract-read-panel__header',
+          {'contract-read-panel__header--expanded' : aciFunction.isExpanded }]"
+        @click="toggle(index)">
+        {{ aciFunction.name }}
+      </header>
+      <div
+        v-show="aciFunction.isExpanded"
+        class="contract-read-panel__content">
+        <p>
+          {{ aciFunction.returns }}
+        </p>
+        <hr>
+        <form @submit.prevent="fetchCall(aciFunction.name, index)">
+          <input
+            v-for="argument in aciFunction.arguments"
+            :id="aciFunction.name + '-' + argument.name"
+            v-model="form[aciFunction.name + '-' + argument.name]"
+            :name="aciFunction.name + '-' + argument.name"
+            :placeholder="argument.type"
+            type="text">
+          <button
+            type="submit">
+            {{ aciFunction.stateful ? 'Call Locally' : 'Send Transaction' }}
+          </button>
+        </form>
+        <hr>
+        <span v-if="response[index]">
+          Response {{ response }}
+        </span>
       </div>
     </div>
   </app-panel>
-  {{ verificationDetails.aci }}
+  <div
+    v-for="(aciFunction, index) in aciFunctions"
+    :key="index"
+    class="contract-read-panel__accordion">
+    {{ aciFunction }}
+    <div/>
+  </div>
+  <hr>
+  form: {{ form }}
 </template>
 
 <script setup>
 import { storeToRefs } from 'pinia'
+import { BigNumber } from 'bignumber.js'
 import { useContractVerifiedStore } from '~/stores/contractVerified'
 import { useAesdk } from '~/stores/aesdk'
 import { useContractDetailsStore } from '@/stores/contractDetails'
@@ -47,29 +112,53 @@ const contractDetailsStore = useContractDetailsStore()
 const { contractDetails } = storeToRefs(contractDetailsStore)
 
 const aciFunctions = ref()
+const aciStatefulFunctions = ref()
+const response = ref([])
+const form = ref({})
 
 onMounted(() => {
   // todo move to store
   const functions = JSON.parse(verificationDetails.value.aci)[3].contract.functions
   functions.forEach(func => {
-    func.isExpanded = false
+    func.isExpanded = true
   })
-  aciFunctions.value = functions
+
+  function myCallback(aciFunction) {
+    return !aciFunction.stateful
+  }
+
+  const result2 = Object.groupBy(functions, myCallback)
+  console.log('result2', result2)
+  aciFunctions.value = result2.false
+  aciStatefulFunctions.value = result2.true
 })
 
 function toggle(index) {
   aciFunctions.value[index].isExpanded = !aciFunctions.value[index].isExpanded
 }
 
-async function fetchFunctionResponse() {
+async function fetchFunctionResponse(functionName, index) {
   console.log('JSON.parse(verificationDetails.value.aci)[3]', JSON.parse(verificationDetails.value.aci)[3])
   const contractInstance = await aeSdk.value.initializeContract({
     aci: [JSON.parse(verificationDetails.value.aci)[3]],
     address: contractDetails.value.id,
   })
-  console.log('contractInstance', contractInstance)
-  const contractCallResult = await contractInstance.read_test_value({ callStatic: true })
-  console.log('contractCallResult?.decodedResult', contractCallResult?.decodedResult)
+  const contractCallResult = await contractInstance[functionName]({ callStatic: true })
+  console.log('contractCallResult?.decodedResult', contractCallResult.decodedResult)
+  response.value = { [index]: new BigNumber(contractCallResult) }
+  console.log('response', response.value)
+}
+
+async function fetchCall(functionName, index) {
+  console.log('JSON.parse(verificationDetails.value.aci)[3]', JSON.parse(verificationDetails.value.aci)[3])
+  const contractInstance = await aeSdk.value.initializeContract({
+    aci: [JSON.parse(verificationDetails.value.aci)[3]],
+    address: contractDetails.value.id,
+  })
+  const contractCallResult = await contractInstance[functionName](form.value['add_test_value-one'], form.value['add_test_value-two'])
+  console.log('contractCallResult?.decodedResult', contractCallResult.decodedResult)
+  response.value = { [index]: new BigNumber(contractCallResult) }
+  console.log('response', response.value)
 }
 </script>
 

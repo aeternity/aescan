6911fd7d07e594f441da8d7b0c897945d0a8a3a0
janmichek <jan@janmichek.cz>
2024-02-08 05:38:18 +0100

    move shared logic to store

--- src/utils/format.js
+++ src/utils/format.js
@@ -159,3 +159,7 @@ export function formatKnownAddress(hash, isEllipsed = true) {
     return hash
   }
 }
+
+export function formatIsStatefulFunction(aciFunction) {
+  return !!aciFunction.stateful
+}

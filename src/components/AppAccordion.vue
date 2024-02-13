0cd3cbc71f77fbaab8b7761fe3a491846358fd4b
janmichek <jan@janmichek.cz>
2024-02-13 04:31:09 +0100

    refactor components structure

--- src/components/AppAccordion.vue
+++ src/components/AppAccordion.vue
@@ -37,7 +37,7 @@ const props = defineProps({
   },
   isDisabled: {
     type: Boolean,
-    default: false,
+    required: true,
   },
 })
 
---
+++ src/components/ContractEntrypointAccordion.vue
@@ -0,0 +1,91 @@
+<template>
+  <!--  todo blank state-->
+  <app-accordion :items="entrypoints">
+    <template #content="{content: {item: aciItem, index}}">
+      <div class="contract-read-panel__container">
+        <p>
+          return type: <i>{{ aciItem.returns }}</i>
+        </p>
+
+        <text-input
+          v-for="(argument, inputIndex) in aciItem.arguments"
+          id="id"
+          :key="inputIndex"
+          v-model="form[aciItem.name + '-' + argument.name]"
+          :placeholder="argument.type"
+          type="text"
+          class="contract-read-panel__input"/>
+
+        <loader-indicator-small v-if="loadingIndex === index"/>
+
+        <app-button
+          v-else
+          type="submit"
+          @click="doIt(aciItem, index, form)">
+          Query
+        </app-button>
+
+        <p
+          v-if="response[index]"
+          :class="[
+            'contract-read-panel__response',
+            {'contract-read-panel__response--error': response[index].responseType === 'error' }
+          ]">
+          {{ response[index].responseType === 'success' ? 'Return value' : 'Error' }}:
+          {{ response[index].message }}
+        </p>
+      </div>
+    </template>
+  </app-accordion>
+</template>
+<script setup>
+
+const form = ref({})
+
+defineProps({
+  entrypoints: {
+    type: Array,
+    required: true,
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  response: {
+    type: Object,
+    default: null,
+  },
+  loadingIndex: {
+    type: Number,
+    default: null,
+  },
+})
+
+const emit = defineEmits([
+  'clicked',
+])
+
+function doIt(aciItem, index) {
+  emit('clicked', aciItem, index, form)
+}
+
+</script>
+
+<style scoped>
+.contract-read-panel {
+  &__container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+  }
+
+  &__response {
+    font-weight: 600;
+
+    &--error {
+      color: red;
+    }
+  }
+}
+</style>

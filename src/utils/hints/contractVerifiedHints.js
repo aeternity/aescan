effd6d967ca779cb3d1cf66ae880cb6af45090c7
Jan Michek <15363559+janmichek@users.noreply.github.com>
2024-02-29 04:25:22 +0100

    Update src/utils/hints/contractVerifiedHints.js
    Co-authored-by: Michele F. <michele-franchi@users.noreply.github.com>

--- src/utils/hints/contractVerifiedHints.js
+++ src/utils/hints/contractVerifiedHints.js
@@ -1,6 +1,6 @@
 export const contractVerifiedHints = {
   license: 'License information for the contract.',
-  compiler: 'Compiler version used for the contract.',
+  compiler: 'Compiler version used for the smart contract.',
   entryFile: 'Main file of the smart contract source code',
   verifiedAt: 'Timestamp when the contract was verified.',
   initCallParameters: 'Initial call parameters for contract.',

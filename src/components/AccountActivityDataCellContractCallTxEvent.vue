1d51da982e669990b49ab481821dd72b0e5f2600
janmichek <jan@janmichek.cz>
2024-04-23 07:38:57 +0200

    show amount label to add_vote activity

--- src/components/AccountActivityDataCellContractCallTxEvent.vue
+++ src/components/AccountActivityDataCellContractCallTxEvent.vue
@@ -6,11 +6,17 @@
   <app-chip size="sm">
     {{ activity.payload.tx.function }}
   </app-chip>
+  <app-chip
+    v-if="activity.payload.tx.function === 'add_vote'"
+    size="sm">
+    {{ formatAePrice(formatAettosToAe(activity.payload.tx.amount)) }}
+  </app-chip>
 </template>
 
 <script setup>
 
 import ValueHashEllipsed from '@/components/ValueHashEllipsed'
+import { formatAePrice, formatAettosToAe } from '@/utils/format'
 
 defineProps({
   activity: {

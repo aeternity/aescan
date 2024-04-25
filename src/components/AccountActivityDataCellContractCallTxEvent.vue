e983441b6b5ada7758954d429c5a69b8606d425a
Jan Michek <15363559+janmichek@users.noreply.github.com>
2024-04-25 13:03:07 +0200

    feat: Show ae amount for add_vote Smart Contract call (#737)

--- src/components/AccountActivityDataCellContractCallTxEvent.vue
+++ src/components/AccountActivityDataCellContractCallTxEvent.vue
@@ -6,11 +6,18 @@
   <app-chip size="sm">
     {{ activity.payload.tx.function }}
   </app-chip>
+
+  <app-chip
+    v-if="activity.payload.tx.amount !== 0"
+    size="sm">
+    {{ formatAePrice(formatAettosToAe(activity.payload.tx.amount)) }}
+  </app-chip>
 </template>
 
 <script setup>
 
 import ValueHashEllipsed from '@/components/ValueHashEllipsed'
+import { formatAePrice, formatAettosToAe } from '@/utils/format'
 
 defineProps({
   activity: {

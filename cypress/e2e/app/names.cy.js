337bc3595a6fbdf2bc23f729a8c266de3cefc7b1
janmichek <jan@janmichek.cz>
2024-05-01 13:08:06 +0200

    add names test

---
+++ cypress/e2e/app/names.cy.js
@@ -0,0 +1,13 @@
+describe('transactions', () => {
+  it('should display names', () => {
+    cy.visit(`${Cypress.env('url')}/names`)
+    cy.get('.names-chart-panel__container').should('be.visible')
+    cy.get('.names-active-panel').should('be.visible')
+
+    cy.contains('.tabs__item', 'In Auction').click()
+    cy.get('.names-in-auction-panel').should('be.visible')
+
+    cy.contains('.tabs__item', 'Expired').click()
+    cy.get('.names-expired-panel').should('be.visible')
+  })
+})

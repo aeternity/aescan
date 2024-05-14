b83badff3d0ea01de0e1468c1b568aa20ad3b06e
janmichek <jan@janmichek.cz>
2024-05-14 10:39:49 +0200

    add N/A label

--- src/components/TokenDetailsPanel.vue
+++ src/components/TokenDetailsPanel.vue
@@ -23,6 +23,7 @@
           </th>
           <td class="token-details-panel__data">
             {{ tokenDetails.name }}
+            <not-available-label v-if="!tokenDetails.name"/>
           </td>
         </tr>
         <tr
@@ -46,7 +47,10 @@
             </hint-tooltip>
           </th>
           <td class="token-details-panel__data">
-            {{ formatNumber(tokenDetails.totalSupply) }} {{ tokenDetails.symbol }}
+            <template v-if="tokenDetails.totalSupply">
+              {{ formatNumber(tokenDetails.totalSupply) }} {{ tokenDetails.symbol }}
+            </template>
+            <not-available-label v-else/>
           </td>
         </tr>
         <tr class="token-details-panel__row">

fcd5567bde0a06e8c0b116afc6de71b27a342f56
janmichek <jan@janmichek.cz>
2024-05-21 11:14:55 +0200

    fix code style

--- src/stores/charts.js
+++ src/stores/charts.js
@@ -74,8 +74,5 @@ export const useChartsStore = defineStore('charts', () => {
     fetchTransactionsStatistics,
     fetchContractsStatistics,
     fetchNamesStatistics,
-    fetchKeyblocksStatistics,
-    fetchTransactionsStatistics,
-    fetchContractsStatistics,
   }
 })

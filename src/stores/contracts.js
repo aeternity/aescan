8525a0f431111523830e0f8215311edd9de905fe
janmichek <jan@janmichek.cz>
2024-05-23 17:33:59 +0200

    fix code style

--- src/stores/contracts.js
+++ src/stores/contracts.js
@@ -4,7 +4,7 @@ import useAxios from '@/composables/useAxios'
 import { adaptContracts } from '@/utils/adapters'
 
 export const useContractsStore = defineStore('contracts', () => {
-  const { MIDDLEWARE_URL, CONTRACT_VERIFICATION_SERVICE_URL } = useRuntimeConfig().public
+  const {MIDDLEWARE_URL, CONTRACT_VERIFICATION_SERVICE_URL} = useRuntimeConfig().public
 
   const axios = useAxios()
   const rawContracts = ref(null)
@@ -18,14 +18,8 @@ export const useContractsStore = defineStore('contracts', () => {
   )
 
   async function fetchContracts(queryParameters = null) {
-    const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || '/v3/transactions?type=contract_create&limit=10'}`)
-const verifiedContracts = await fetchVerifiedContracts(data)
-    if (verifiedContracts) {
-      data.data.forEach(contract => {
-        contract.isVerified = !!verifiedContracts.find(verifiedContract => verifiedContract === contract.tx.contractId)
-      })
-    }
-    const verifiedContracts = await fetchVerfiedContracts(data)
+    const {data} = await axios.get(`${MIDDLEWARE_URL}${queryParameters || '/v3/transactions?type=contract_create&limit=10'}`)
+    const verifiedContracts = await fetchVerifiedContracts(data)
     if (verifiedContracts) {
       data.data.forEach(contract => {
         contract.isVerified = !!verifiedContracts.find(verifiedContract => verifiedContract === contract.tx.contractId)
@@ -36,14 +30,14 @@ const verifiedContracts = await fetchVerifiedContracts(data)
 
   async function fetchContractsCount() {
     contractsCount.value = null
-    const { data } = await axios.get(`${MIDDLEWARE_URL}/v3/transactions/count?type=contract_create`)
+    const {data} = await axios.get(`${MIDDLEWARE_URL}/v3/transactions/count?type=contract_create`)
     contractsCount.value = data
   }
 
   async function fetchVerifiedContracts(contracts) {
     const slug = contracts.data.map(contract => contract.tx.contractId).join('&ids=')
     try {
-      const { data } = await axios.get(`${CONTRACT_VERIFICATION_SERVICE_URL}/contracts?ids=${slug}`)
+      const {data} = await axios.get(`${CONTRACT_VERIFICATION_SERVICE_URL}/contracts?ids=${slug}`)
       return data.contracts.map(contract => contract.contractId)
     } catch (error) {
       return null

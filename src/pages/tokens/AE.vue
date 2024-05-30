cfd169540d6183ab973f50c934c4d561ec126570
janmichek <jan@janmichek.cz>
2024-05-30 16:56:04 +0200

    add page file

---
+++ src/pages/tokens/AE.vue
@@ -0,0 +1,23 @@
+<template>
+  <Head>
+    <Title>AE Coin</Title>
+  </Head>
+
+  <page-header>
+    AE Coin
+    <template #tooltip>
+      {{ tokensHints.token }}
+    </template>
+  </page-header>
+  <template v-if="!isLoading">
+    <ae-coin-panel/>
+  </template>
+  <loader-panel v-else/>
+</template>
+
+<script setup>
+import PageHeader from '@/components/PageHeader'
+import { tokensHints } from '@/utils/hints/tokensHints'
+
+const { isLoading } = useLoading()
+</script>

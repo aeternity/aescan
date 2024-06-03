e219317fb1aee82bd24386151ad144b45ac6c726
janmichek <jan@janmichek.cz>
2024-06-03 10:37:13 +0200

    fix displaying status

--- src/components/SearchNamesTable.vue
+++ src/components/SearchNamesTable.vue
@@ -25,7 +25,7 @@
             {{ name.name }}
           </app-link>
         </td>
-        <td>{{ name.status ? 'Active' : 'Expired' }}</td>
+        <td>{{ name.active ? 'Active' : 'Expired' }}</td>
       </tr>
     </tbody>
   </table>

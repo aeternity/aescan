e882564090dd9b981bba45664f268f45ed7d6724
janmichek <jan@janmichek.cz>
2024-06-03 17:12:36 +0200

    cover feature with a test

--- cypress/e2e/app/aeCoin.cy.js
+++ cypress/e2e/app/aeCoin.cy.js
@@ -3,5 +3,6 @@ describe('ae coin', () => {
     cy.visit('/tokens/ae')
 
     cy.get('.ae-coin-panel table').should('be.visible')
+    cy.get('.ae-coin-transactions-panel .paginated-content').should('be.visible')
   })
 })

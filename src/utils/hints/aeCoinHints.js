3f944984b34000fbd025df63d27e31b5f7cc7cf6
janmichek <jan@janmichek.cz>
2024-06-12 11:07:57 +0200

    fix hints after rebase

--- src/utils/hints/aeCoinHints.js
+++ src/utils/hints/aeCoinHints.js
@@ -1,7 +1,7 @@
 export const aeCoinHints = {
   aeCoin: 'æternity native coin.',
-  tokenSymbol: 'Reserved token name.',
-  price: 'Price of the token in AE and USD.',
+  tokenSymbol: 'Reserved coin name.',
+  price: 'Price of the coin in AE and USD.',
   totalSupply: 'Amount of coins in circulation.',
   circulating: 'Amount of distributed coins minus the burned amount.',
   decimals: 'The decimal granularity of the coin.',

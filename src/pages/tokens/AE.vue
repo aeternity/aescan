149d0b526a55654475ae0f7d062a0a0c585ff8cc
Jan Michek <15363559+janmichek@users.noreply.github.com>
2024-06-12 11:03:29 +0200

    feat: Ae coin transactions (#831)
    Co-authored-by: Michele F. <michele-franchi@users.noreply.github.com>

--- src/pages/tokens/AE.vue
+++ src/pages/tokens/AE.vue
@@ -9,15 +9,30 @@
       {{ aeCoinHints.aeCoin }}
     </template>
   </page-header>
+
   <template v-if="!isLoading">
-    <ae-coin-panel/>
+    <ae-coin-panel class="ae-coin__panel"/>
+    <app-tabs>
+      <app-tab title="Transactions">
+        <ae-coin-transactions-panel/>
+      </app-tab>
+    </app-tabs>
   </template>
   <loader-panel v-else/>
 </template>
 
 <script setup>
-import PageHeader from '@/components/PageHeader'
 import { aeCoinHints } from '@/utils/hints/aeCoinHints'
 
 const { isLoading } = useLoading()
 </script>
+
+<style scoped>
+.ae-coin__panel {
+  margin-bottom: var(--space-4);
+
+  @media (--desktop) {
+    margin-bottom: var(--space-6);
+  }
+}
+</style>

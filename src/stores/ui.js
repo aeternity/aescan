d8b86984a36d608aab3f3dcefeba1e596b6e1957
janmichek <jan@janmichek.cz>
2024-06-13 14:32:20 +0200

    merge stores

--- src/stores/ui.js
+++ src/stores/ui.js
@@ -1,8 +1,16 @@
 import { defineStore } from 'pinia'
+
 export const useUiStore = defineStore('ui', () => {
   const isMobileMenuOpen = ref(false)
+  const timeFormat = ref('relative')
+
+  function toggleTimeFormat() {
+    timeFormat.value = timeFormat.value === 'absolute' ? 'relative' : 'absolute'
+  }
 
   return {
     isMobileMenuOpen,
+    timeFormat,
+    toggleTimeFormat,
   }
 })

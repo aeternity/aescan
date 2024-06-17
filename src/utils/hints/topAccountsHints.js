8e2a268dc6a44b3d0e3b09f2f413e86253ee2941
janmichek <jan@janmichek.cz>
2024-06-17 11:00:58 +0200

    fix typo

--- src/utils/hints/topAccountsHints.js
+++ src/utils/hints/topAccountsHints.js
@@ -1,7 +1,7 @@
 export const topAccountsHints = {
   topAccounts: 'The top accounts are the accounts with the highest balance of AE coins.',
   rank: 'Rank of the account in the top accounts list.',
-  balance: 'Amount of AE tokens held by the account',
+  balance: 'Amount of AE tokens held by the account.',
   account: 'Account address.',
   percentage: 'Percentage of the total supply of AE coins that the account holds.',
 }

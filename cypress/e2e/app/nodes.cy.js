f535cc70104a428acbc79209b1686b27147af766
janmichek <jan@janmichek.cz>
2024-07-15 16:03:50 +0200

    fix test

--- cypress/e2e/app/nodes.cy.js
+++ cypress/e2e/app/nodes.cy.js
@@ -1,7 +1,7 @@
 describe('nodes', () => {
   it('should display nodes', () => {
-    cy.visit('/nodes/')
+    cy.visit('/nodes')
 
-    cy.get('.nodes-tables').should('be.visible')
+    cy.get('.nodes-table').should('be.visible')
   })
 })

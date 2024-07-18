a3401e6cfbcbfb29d57768d8315ed4ced0e35b00
janmichek <jan@janmichek.cz>
2024-07-18 13:45:26 +0200

    fix indent

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,26 +5,26 @@ import {sentryVitePlugin} from '@sentry/vite-plugin'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: './src',
-    css: ['@/assets/styles/main.css'],
-    devServer: {
-        port: 8080,
+  css: ['@/assets/styles/main.css'],
+  devServer: {
+    port: 8080,
+  },
+  nitro: {
+    routeRules: {
+      '/proxy/nodes': {proxy: 'http://138.68.22.27:3113/v2/debug/network', cors: true},
+      '/proxy/gate': {proxy: 'https://api.gateio.ws/api/v4/spot/tickers?currency_pair=AE_USDT', cors: true},
+      '/proxy/mexc': {proxy: 'https://api.mexc.com/api/v3/ticker/24hr?symbol=AEUSDT', cors: true},
+      '/proxy/coinw': {proxy: 'https://api.coinw.com/api/v1/public?command=returnTicker', cors: true},
     },
-    nitro: {
-        routeRules: {
-            '/proxy/nodes': {proxy: 'http://138.68.22.27:3113/v2/debug/network', cors: true},
-            '/proxy/gate': {proxy: 'https://api.gateio.ws/api/v4/spot/tickers?currency_pair=AE_USDT', cors: true},
-            '/proxy/mexc': {proxy: 'https://api.mexc.com/api/v3/ticker/24hr?symbol=AEUSDT', cors: true},
-            '/proxy/coinw': {proxy: 'https://api.coinw.com/api/v1/public?command=returnTicker', cors: true},
-        },
-    },
-    modules: [
-        '@pinia/nuxt',
-        '@nuxtjs/plausible',
-        'nuxt-monaco-editor',
-        'nuxt-booster',
-    ],
-    imports: {
-        dirs: ['./stores'],
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/plausible',
+    'nuxt-monaco-editor',
+    'nuxt-booster',
+  ],
+  imports: {
+    dirs: ['./stores'],
   },
   plausible: {
     apiHost: 'https://plausible.dev.service.aepps.com',
@@ -62,54 +62,54 @@ export default defineNuxtConfig({
       'postcss-custom-media': {},
       'postcss-import': {},
       'postcss-nested': {},
-        },
     },
-    sourcemap: true,
-    vite: {
-        build: {target: 'es2020'},
-        optimizeDeps: {
-            esbuildOptions: {target: 'es2020'},
-        },
-        define: {
-            __VUE_PROD_DEVTOOLS__: process.env.MODE !== 'production',
-        },
-        plugins: [
-            {
-                name: 'icon-svg-loader',
-                enforce: 'pre',
-                async load(path) {
-                    if (!path.match(/icons\/.*.svg$/)) {
-                        return
-                    }
+  },
+  sourcemap: true,
+  vite: {
+    build: {target: 'es2020'},
+    optimizeDeps: {
+      esbuildOptions: {target: 'es2020'},
+    },
+    define: {
+      __VUE_PROD_DEVTOOLS__: process.env.MODE !== 'production',
+    },
+    plugins: [
+      {
+        name: 'icon-svg-loader',
+        enforce: 'pre',
+        async load(path) {
+          if (!path.match(/icons\/.*.svg$/)) {
+            return
+          }
 
-                    const {code} = compileTemplate({
-                        id: JSON.stringify(path),
-                        filename: path,
-                        source: await fs.readFile(path, 'utf-8'),
-                        transformAssetUrls: false,
-                    })
+          const {code} = compileTemplate({
+            id: JSON.stringify(path),
+            filename: path,
+            source: await fs.readFile(path, 'utf-8'),
+            transformAssetUrls: false,
+          })
 
-                    return `${code}\nexport default { render: render }`
-                },
-            },
-            sentryVitePlugin({
-                authToken: process.env.SENTRY_AUTH_TOKEN,
-                org: 'sentry',
-                project: 'aescan-develop',
-                url: 'https://sentry.dev.service.aepps.com/',
-            }),
-            sentryVitePlugin({
-                authToken: process.env.SENTRY_AUTH_TOKEN,
-                org: 'sentry',
-                project: 'aescan-production',
-                url: 'https://sentry.dev.service.aepps.com/',
-            }),
-        ],
-    },
-    monacoEditor: {
-        locale: 'en',
-        componentName: {
-            codeEditor: 'MonacoEditor',
+          return `${code}\nexport default { render: render }`
         },
+      },
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: 'sentry',
+        project: 'aescan-develop',
+        url: 'https://sentry.dev.service.aepps.com/',
+      }),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: 'sentry',
+        project: 'aescan-production',
+        url: 'https://sentry.dev.service.aepps.com/',
+      }),
+    ],
+  },
+  monacoEditor: {
+    locale: 'en',
+    componentName: {
+      codeEditor: 'MonacoEditor',
     },
+  },
 })

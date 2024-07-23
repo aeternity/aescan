ff58881efdc60a773eea73e40c26cfc5c66dcb37
janmichek <jan@janmichek.cz>
2024-07-23 17:45:36 +0200

    remove duplicate functions

--- src/utils/format.js
+++ src/utils/format.js
@@ -127,48 +127,11 @@ export function formatNameState(name, blockHeight) {
   }
 }
 
-export function formatNameState(name, blockHeight) {
-  const isInAuction = name.status === 'auction'
-  const isActive = name.active
-  const isExpired = name.status === 'name' && !name.active
-  const isRevoked = isExpired && name.active === false &&
-    name.info.expireHeight + REVOKED_PERIOD > blockHeight
-
-  if (isInAuction) {
-    return 'auction'
-  } else if (isRevoked) {
-    return 'revoked'
-  } else if (isExpired) {
-    return 'expired'
-  } else if (isActive) {
-    return 'active'
-  }
-}
-
 export function formatIsAuction(name) {
   const auctionLength = 13
   const suffixLength = 6
   return name.length - suffixLength < auctionLength
 }
-
-export function formatNameState(name, blockHeight) {
-  const isInAuction = name.status === 'auction'
-  const isActive = name.active
-  const isExpired = name.status === 'name' && !name.active
-  const isRevoked = isExpired && name.active === false &&
-    name.info.expireHeight + REVOKED_PERIOD > blockHeight
-
-  if (isInAuction) {
-    return 'auction'
-  } else if (isRevoked) {
-    return 'revoked'
-  } else if (isExpired) {
-    return 'expired'
-  } else if (isActive) {
-    return 'active'
-  }
-}
-
 
 
 export function formatPercentage(percentage) {

ec35e1f6b275ce798d579de2686d2048d9cadbe3
janmichek <jan@janmichek.cz>
2024-07-30 15:14:01 +0200

    fix code style

--- src/components/AccountTokensTable.vue
+++ src/components/AccountTokensTable.vue
@@ -1,57 +1,57 @@
 <template>
   <table>
     <thead>
-    <tr>
-      <th>
-        Amount
-        <hint-tooltip>
-          {{ tokensHints.amount }}
-        </hint-tooltip>
-      </th>
-      <th>
-        Value
-        <hint-tooltip>
-          {{ tokensHints.value }}
-        </hint-tooltip>
-      </th>
-      <th>
-        Name
-        <hint-tooltip>
-          {{ tokensHints.tokenName }}
-        </hint-tooltip>
-      </th>
-      <th>
-        Smart Contract
-        <hint-tooltip>
-          {{ tokensHints.smartContractId }}
-        </hint-tooltip>
-      </th>
-    </tr>
+      <tr>
+        <th>
+          Amount
+          <hint-tooltip>
+            {{ tokensHints.amount }}
+          </hint-tooltip>
+        </th>
+        <th>
+          Value
+          <hint-tooltip>
+            {{ tokensHints.value }}
+          </hint-tooltip>
+        </th>
+        <th>
+          Name
+          <hint-tooltip>
+            {{ tokensHints.tokenName }}
+          </hint-tooltip>
+        </th>
+        <th>
+          Smart Contract
+          <hint-tooltip>
+            {{ tokensHints.smartContractId }}
+          </hint-tooltip>
+        </th>
+      </tr>
     </thead>
     <tbody>
-    <tr
-      v-for="token in accountTokens?.data"
-      :key="token.contractId">
-      <td>
-        <price-label
-          :price="token.amount"
-          :contract-id="token.contractId"
-          :currency="token.tokenSymbol"/>
-      </td>
-      <td>
-        {{ token.value }}
-      </td>
-      <td>
-        <app-link :to="`/tokens/${token.contractId}`">
-          {{ token.tokenName }}
-        </app-link>
-      </td>
-      <td>
-        <value-hash-ellipsed
-          :link-to="`/contracts/${token.contractId}`"
-          :hash="token.contractId"/>
-      </td>
-    </tr>
+      <tr
+        v-for="token in accountTokens?.data"
+        :key="token.contractId">
+        <td>
+          <price-label
+            :price="token.amount"
+            :contract-id="token.contractId"
+            :currency="token.tokenSymbol"/>
+        </td>
+        <td>
+          {{ token.value }}
+        </td>
+        <td>
+          <app-link :to="`/tokens/${token.contractId}`">
+            {{ token.tokenName }}
+          </app-link>
+        </td>
+        <td>
+          <value-hash-ellipsed
+            :link-to="`/contracts/${token.contractId}`"
+            :hash="token.contractId"/>
+        </td>
+      </tr>
     </tbody>
   </table>
 </template>

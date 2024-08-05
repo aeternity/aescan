83124624d70c2ef7d1a8baf14194a38dafae3d31
janmichek <jan@janmichek.cz>
2024-08-05 13:19:14 +0200

    add hints

---
+++ src/utils/hints/dexTradesHints.js
@@ -0,0 +1,9 @@
+export const dexTradesHints = {
+  hash: 'Unique identifier of the transaction.',
+  executed: 'Exact date and time when the trade was executed.',
+  action: 'Uses Wrapped AE as base currency if they are part of the trading pair and Wrapped AE otherwise.',
+  tokenAmountIn: 'Amount of tokens put at DEX.',
+  tokenAmountOut: 'Amount of tokens received at DEX.',
+  swappedRate: 'The exchange rate at which the transaction was made.',
+  value: 'Total transaction value in USD.',
+}

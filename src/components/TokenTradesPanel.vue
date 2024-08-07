5497dc017d016fe5131fb45860d73015d03d4fa9
janmichek <jan@janmichek.cz>
2024-08-07 09:44:02 +0200

    add mobile view

--- src/components/TokenTradesPanel.vue
+++ src/components/TokenTradesPanel.vue
@@ -1,159 +1,40 @@
 <template>
-
-
-  <!--    <paginated-content-->
-  <!--      :entities="tokenEvents"-->
-  <!--      pagination-style="history"-->
-  <!--      :total-count="tokenEventsCount"-->
-  <!--      @prev-clicked="loadPrevEvents"-->
-  <!--      @next-clicked="loadNextEvents">-->
-  <!--      <token-events-table-->
-  <!--        v-if="tokenEvents"-->
-  <!--        :token-events="tokenEvents"-->
-  <!--        class="token-events-panel__table u-hidden-mobile"/>-->
-  <!--      <token-events-table-condensed-->
-  <!--        v-if="tokenEvents"-->
-  <!--        :token-events="tokenEvents"-->
-  <!--        class="u-hidden-desktop"/>-->
-  <!--    </paginated-content>-->
-
-
-  <table class="dex-trades-table">
-    <tr>
-      <th>
-        Hash
-        <hint-tooltip>
-          {{ dexTradesHints.hash }}
-        </hint-tooltip>
-      </th>
-      <th>
-        <time-toggle-button>Executed</time-toggle-button>
-        <hint-tooltip>
-          {{ dexTradesHints.executed }}
-        </hint-tooltip>
-      </th>
-      <th>
-        Action
-        <hint-tooltip>
-          {{ dexTradesHints.action }}
-        </hint-tooltip>
-      </th>
-
-      <th>
-        Token Amount (Out)
-        <hint-tooltip>
-          {{ dexTradesHints.tokenAmountOut }}
-        </hint-tooltip>
-      </th>
-
-      <th>
-        Token Amount (In)
-        <hint-tooltip>
-          {{ dexTradesHints.tokenAmountIn }}
-        </hint-tooltip>
-      </th>
-      <th>
-        Swapped Rate
-        <hint-tooltip>
-          {{ dexTradesHints.swappedRate }}
-        </hint-tooltip>
-      </th>
-      <th>
-        Value
-        <hint-tooltip>
-          {{ dexTradesHints.value }}
-        </hint-tooltip>
-      </th>
-    </tr>
-
-    <tr
-      v-for="trade in trades.data"
-      :key="trade.txHash">
-      <td>
-        <value-hash-ellipsed
-          :link-to="`/transactions/${trade.txHash}`"
-          :hash="trade.txHash"/>
-      </td>
-      <td>
-        <block-time-cell
-          :height="trade.height"
-          :timestamp="trade.timestamp"/>
-      </td>
-      <td>
-        <app-chip :variant="getChipVariant(trade.action)">
-          {{ trade.action }}
-        </app-chip>
-      </td>
-      <td>
-        <price-label
-          :contract-id="trade.fromContract"
-          :currency="trade.fromToken"
-          :price="trade.fromAmount"
-          :max-digits="4"
-          :has-link="true"/>
-      </td>
-      <td>
-        <price-label
-          :contract-id="trade.toContract"
-          :currency="trade.toToken"
-          :price="trade.toAmount"
-          :max-digits="4"
-          :has-link="true"/>
-      </td>
-      <td>
-        <not-available-label v-if="!trade.rate"/>
-        {{ trade.rate }}
-      </td>
-      <td>
-        <not-available-label v-if="trade.value === '0'"/>
-        <template v-else>
-          $ {{ trade.value }}
-        </template>
-      </td>
-    </tr>
-  </table>
-
-
-  {{ trades }}
+  <app-panel>
+    <paginated-content
+      :entities="trades"
+      pagination-style="history"
+      @prev-clicked="loadPrevEvents"
+      @next-clicked="loadNextEvents">
+      <dex-trades-table
+        :trades="trades"
+        class="u-hidden-mobile"/>
+      <dex-trades-table-condensed
+        :trades="trades"
+        class="u-hidden-desktop"/>
+    </paginated-content>
+  </app-panel>
 </template>
-
 <script setup>
-import { storeToRefs } from 'pinia'
-import { useTokenDetailsStore } from '@/stores/tokenDetails'
 
-const { trades } = storeToRefs(useTokenDetailsStore())
-const { fetchTokenTrades } = useTokenDetailsStore()
+const tokenDetailsStore = useTokenDetailsStore()
+const { trades } = storeToRefs(tokenDetailsStore)
+const { fetchTokenTrades } = tokenDetailsStore
 const route = useRoute()
-//
-// function loadPrevEvents() {
-//   fetchTokenEvents({ queryParameters: tokenEvents.value.prev })
-// }
-//
-// function loadNextEvents() {
-//   fetchTokenEvents({ queryParameters: tokenEvents.value.next })
-// }
 
 
-function getChipVariant(action) {
-  const variants = {
-    BUY: 'error',
-    SELL: 'success',
-    SWAP: 'primary',
-  }
-  return variants[action]
+function loadPrevEvents() {
+  fetchTokenTrades({ queryParameters: trades.value.prev })
+}
+
+function loadNextEvents() {
+  fetchTokenTrades({ queryParameters: trades.value.next })
 }
 
 if (process.client) {
   const limit = computed(() => isDesktop() ? 10 : 3)
   await fetchTokenTrades({
-    limit: limit.value,
     contractId: route.params.id,
+    limit: limit.value,
   })
 }
 </script>
-
-<style scoped>
-.token-events-panel__table {
-  margin-bottom: var(--space-4);
-}
-</style>

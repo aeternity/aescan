d5b1b3b797bbb3a07ad7cbe2a223f37883cfd63a
janmichek <jan@janmichek.cz>
2024-08-26 13:34:38 +0200

    use corresponding network name

--- src/stores/aesdk.js
+++ src/stores/aesdk.js
@@ -3,12 +3,12 @@ import { useRuntimeConfig } from 'nuxt/app'
 import { AeSdk, Node } from '@aeternity/aepp-sdk'
 
 export const useAesdk = defineStore('aesdk', () => {
-  const { NODE_URL } = useRuntimeConfig().public
+  const { NODE_URL, NETWORK_ID } = useRuntimeConfig().public
 
   const node = new Node(NODE_URL, { ignoreVersion: true })
   const aeSdk = new AeSdk({
     nodes: [
-      { name: 'mainnet', instance: node },
+      { name: NETWORK_ID, instance: node },
     ],
     compilerUrl: 'https://compiler.aepps.com',
   })

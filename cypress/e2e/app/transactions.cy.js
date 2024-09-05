d32ee92c74f7517f79f9f17e6e16509501602450
janmichek <jan@janmichek.cz>
2024-09-05 16:43:04 +0200

    improve testing

--- cypress/e2e/app/transactions.cy.js
+++ cypress/e2e/app/transactions.cy.js
@@ -5,5 +5,27 @@ describe('transactions', () => {
     cy.get('.transactions-table').should('be.visible')
     cy.get('.line-chart').should('be.visible')
     cy.get('.transaction-statistics__panel').should('be.visible')
+    cy.get('.transaction-statistics__panel .chip').should('be.visible')
+  })
+
+  it('should display same transactions after going back from detail', () => {
+    cy.visit('/transactions')
+
+    cy.get('.transactions-table__data').first()
+      .invoke('text')
+      .then(hash1 => {
+        const transactionDetailLink = cy.get('.transactions-table__data').first().find('.u-hidden-mobile a')
+        transactionDetailLink.click()
+
+        cy.get('.copy-chip__text') // workaround to time out before going back get the element
+
+        cy.go(-1)
+
+        cy.get('.transactions-table__data').first()
+          .invoke('text')
+          .should(hash2 => {
+            expect(hash1).to.eq(hash2)
+          })
+      })
   })
 })

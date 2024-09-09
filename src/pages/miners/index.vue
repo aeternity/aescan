894a41ee927105ccb9baf288255392bcf88e7214
janmichek <jan@janmichek.cz>
2024-09-09 11:29:28 +0200

    add basic UI

---
+++ src/pages/miners/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Head>
+    <Title>Miners</Title>
+  </Head>
+
+  <page-header>
+    Miners
+    <template #tooltip>
+      <!--      todo hints-->
+      {{ topAccountsHints.topAccounts }}
+    </template>
+  </page-header>
+  <miners-panel v-if="!isLoading"/>
+  <loader-panel v-else/>
+</template>
+
+<script setup>
+import PageHeader from '~/components/PageHeader.vue'
+import { topAccountsHints } from '~/utils/hints/topAccountsHints'
+
+const { isLoading } = useLoading()
+</script>

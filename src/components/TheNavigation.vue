4fff380ddd8c25babb09721ce88aa7d2497d73a1
janmichek <jan@janmichek.cz>
2024-09-09 17:54:05 +0200

    format values

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -59,8 +59,8 @@ const menuOptions = ref([{
       path: '/nodes',
     },
     {
-      name: 'Miners',
-      path: '/miners',
+      name: 'Mining Pools',
+      path: '/mining-pools',
     },
   ],
 },

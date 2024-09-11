5af6305443a20ac9a1958f7cb82af169d11ec3ca
janmichek <jan@janmichek.cz>
2024-09-11 12:30:26 +0200

    fix displaying values

--- src/components/MinersStatistics.vue
+++ src/components/MinersStatistics.vue
@@ -1,23 +1,69 @@
 <template>
+  {{ know }}
+  <br>
+  <br>
+  <br>
+  status
+  {{ status }}
   <div class="transaction-statistics">
     <app-panel class="transaction-statistics__panel">
-      <h5>MINERS COUNT</h5>
+      <h5>MINERS</h5>
       <div class="transaction-statistics__value">
         {{ minersCount }}
       </div>
     </app-panel>
     <app-panel class="transaction-statistics__panel">
-      <h5>REWARD</h5>
+      <h5>PEERS</h5>
       <div class="transaction-statistics__value">
-        <!--        <price-label :price="formatAePrice(blockReward)"/>-->
+        {{ know.peers }}
+      </div>
+    </app-panel>
+    <app-panel class="transaction-statistics__panel">
+      <h5>POOLS</h5>
+      <div class="transaction-statistics__value">
+        {{ MINERS.length }}
+      </div>
+    </app-panel>
+    <app-panel class="transaction-statistics__panel">
+      <h5>REWARD</h5>
+      <!--      todo why reward is different_-->
+      <div class="transaction-statistics__value">
+        <!--        <price-label :price="blockReward"/>-->
         <price-label :price="formatAettosToAe(blockReward, null)"/>
       </div>
     </app-panel>
     <app-panel class="transaction-statistics__panel">
       <h5>DIFFICULTY</h5>
       <div class="transaction-statistics__value">
-        {{ formatNumber(difficulty, 0, 0, 4) }} G
+        {{ formatNumber(status.difficulty, 0, 0, 4) }} G
       </div>
+    </app-panel>
+
+    <app-panel class="transaction-statistics__panel">
+      <h5>TOTAL MINED</h5>
+      <div class="transaction-statistics__value">
+        TOTAAAL
+      </div>
+    </app-panel>
+    <app-panel class="transaction-statistics__panel">
+      <h5>BLOCK TIME</h5>
+      <div class="transaction-statistics__value">
+        {{ blocksPerMinute }} mins/block
+      </div>
+    </app-panel>
+    <app-panel class="transaction-statistics__panel">
+      <h5>HASHRATE</h5>
+      <div class="transaction-statistics__value">
+        {{ formatNumber(status.hashrate, 0, 0, 4) }} G
+      </div>
+    </app-panel>
+    <app-panel class="transaction-statistics__panel">
+      <h5>nr of blocks</h5>
+      {{ status.topBlock }}
+    </app-panel>
+    <app-panel class="transaction-statistics__panel">
+      <h5>nr of blocks</h5>
+      <pie-chart/>
     </app-panel>
   </div>
 </template>
@@ -26,7 +72,7 @@ import { useMinersStore } from '@/stores/miners'
 import { formatAettosToAe } from '~/utils/format'
 
 const { fetchMiners } = useMinersStore()
-const { minersCount, blockReward, difficulty } = storeToRefs(useMinersStore())
+const { minersCount, blockReward, difficulty, know, status, blocksPerMinute } = storeToRefs(useMinersStore())
 
 if (process.client) {
   await fetchMiners()
@@ -37,6 +83,7 @@ if (process.client) {
 .transaction-statistics {
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   gap: var(--space-2);
   width: 100%;
   margin-bottom: var(--space-2);
@@ -47,10 +94,10 @@ if (process.client) {
 
   &__panel {
     padding: var(--space-4);
-    width: 100%;
+    width: 50%;
 
     @media (--desktop) {
-      width: 50%;
+      width: 18%;
     }
   }
 

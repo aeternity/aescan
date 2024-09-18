fef8a6ee5f856fec2ae657c72c865a1b8fe5d597
janmichek <jan@janmichek.cz>
2024-09-18 18:30:38 +0200

    add row highlighting

--- src/components/NodesTable.vue
+++ src/components/NodesTable.vue
@@ -1,76 +1,80 @@
 <template>
   <table class="nodes-table">
-    <tr>
-      <th class="nodes-table__header">
-        Node Id
-        <hint-tooltip>
-          {{ nodesHints.nodeId }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        Host
-        <hint-tooltip>
-          {{ nodesHints.host }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        Port
-        <hint-tooltip>
-          {{ nodesHints.port }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        Network Id
-        <hint-tooltip>
-          {{ nodesHints.networkId }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        Version
-        <hint-tooltip>
-          {{ nodesHints.nodeVersion }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        Revision
-        <hint-tooltip>
-          {{ nodesHints.nodeRevision }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        Vendor
-        <hint-tooltip>
-          {{ nodesHints.nodeVendor }}
-        </hint-tooltip>
-      </th>
-      <th class="nodes-table__header">
-        OS
-        <hint-tooltip>
-          {{ nodesHints.os }}
-        </hint-tooltip>
-      </th>
-    </tr>
-    <tr
-      v-for="(node, index) in nodes"
-      :key="node.networkId">
-      <td class="nodes-table__hash">
-        <value-hash-ellipsed :hash="index"/>
-      </td>
-      <td>{{ node.host }}</td>
-      <td>{{ node.port }}</td>
-      <td>{{ formatNullable(node.networkId) }}</td>
-      <td>{{ formatNullable(node.nodeVersion) }}</td>
-      <td>
-        <value-hash-ellipsed
-          v-if="node.nodeRevision"
-          :hash="node.nodeRevision"/>
-        <template v-else>
-          N/A
-        </template>
-      </td>
-      <td>{{ formatNullable(node.nodeVendor) }}</td>
-      <td>{{ formatNullable(node.nodeOs) }}</td>
-    </tr>
+    <thead>
+      <tr>
+        <th class="nodes-table__header">
+          Node Id
+          <hint-tooltip>
+            {{ nodesHints.nodeId }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          Host
+          <hint-tooltip>
+            {{ nodesHints.host }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          Port
+          <hint-tooltip>
+            {{ nodesHints.port }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          Network Id
+          <hint-tooltip>
+            {{ nodesHints.networkId }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          Version
+          <hint-tooltip>
+            {{ nodesHints.nodeVersion }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          Revision
+          <hint-tooltip>
+            {{ nodesHints.nodeRevision }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          Vendor
+          <hint-tooltip>
+            {{ nodesHints.nodeVendor }}
+          </hint-tooltip>
+        </th>
+        <th class="nodes-table__header">
+          OS
+          <hint-tooltip>
+            {{ nodesHints.os }}
+          </hint-tooltip>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(node, index) in nodes"
+        :key="node.networkId">
+        <td class="nodes-table__hash">
+          <value-hash-ellipsed :hash="index"/>
+        </td>
+        <td>{{ node.host }}</td>
+        <td>{{ node.port }}</td>
+        <td>{{ formatNullable(node.networkId) }}</td>
+        <td>{{ formatNullable(node.nodeVersion) }}</td>
+        <td>
+          <value-hash-ellipsed
+            v-if="node.nodeRevision"
+            :hash="node.nodeRevision"/>
+          <template v-else>
+            N/A
+          </template>
+        </td>
+        <td>{{ formatNullable(node.nodeVendor) }}</td>
+        <td>{{ formatNullable(node.nodeOs) }}</td>
+      </tr>
+    </tbody>
   </table>
 </template>
 

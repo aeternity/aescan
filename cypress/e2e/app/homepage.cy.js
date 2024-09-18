6cd17ce48006a265e86b2ff4deef108971172114
janmichek <jan@janmichek.cz>
2024-09-18 22:52:34 +0200

    fix content checking for test

--- cypress/e2e/app/homepage.cy.js
+++ cypress/e2e/app/homepage.cy.js
@@ -1,18 +1,19 @@
 describe('homepage', () => {
   it('should display homepage', () => {
     cy.visit('/')
-
+    // todo improve satisfy
     cy.get('.stats-panel').should('be.visible')
     cy.get('.dashboard-state-channels-panel table').should('be.visible')
     cy.get('.dashboard-auctions-panel')
-      .should('satisfy',
-        elements => {
-          return elements[0].childNodes.forEach(element => {
-            if (element.localName === 'table' || element.className === 'blank-state') {
-              return true
-            }
-          })
+      .should('satisfy', elements => {
+        let hasContent = false
+        elements[0].childNodes.forEach(element => {
+          if (element.localName === 'table' || element.className === 'blank-state') {
+            hasContent = true
+          }
         })
+        return hasContent
+      })
     cy.get('.dashboard-names-panel table').should('be.visible')
     cy.get('.dashboard-keyblock-panel table').should('be.visible')
     cy.get('.search-bar').should('be.visible')

93966f977ab22be0a361567c959d6b1b0bdaf67a
janmichek <jan@janmichek.cz>
2024-09-19 11:30:09 +0200

    fix currency printing

--- src/utils/format.js
+++ src/utils/format.js
@@ -8,7 +8,6 @@ import {
   REVOKED_PERIOD,
 } from '@/utils/constants'
 
-
 export function formatEllipseHash(hash) {
   const prefix = hash.slice(0, 10)
   const suffix = hash.slice(hash.length - 2)
@@ -36,13 +35,13 @@ export function formatNumber(
   }).format(number)
 }
 
-export function formatAePrice(price, maxDigits = 8) {
+export function formatAePrice(price, maxDigits = 8, currency = 'AE') {
   if (isNaN(price) || price === null) {
     return price
   }
 
   if (maxDigits === null) {
-    return `${formatNumber(price, 0, MAXIMUM_FRACTION_DIGITS)}`
+    return `${formatNumber(price, 0, MAXIMUM_FRACTION_DIGITS)} ${currency}`
   }
 
   const truncatedPrice = new BigNumber(price).toFixed(
@@ -58,16 +57,16 @@ export function formatAePrice(price, maxDigits = 8) {
 
   if (!decimals) {
     if (price === 0) {
-      return `0`
+      return `0 ${currency}`
     }
     if (integers === '0') {
-      return `~${integers}`
+      return `~${integers} ${currency}`
     } else {
-      return `${formatNumber(integers)}`
+      return `${formatNumber(integers)} ${currency}`
     }
   }
 
-  return `${formatNumber(truncatedPrice, decimals.length, maxDigits)}`
+  return `${formatNumber(truncatedPrice, decimals.length, maxDigits)} ${currency}`
 }
 
 export function formatReduceDecimals(tokenAmount, numberOfDecimals) {
@@ -218,4 +217,3 @@ export function formatEntrypointResponse(value, type) {
   }
   return value
 }
-

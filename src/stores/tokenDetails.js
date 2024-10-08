9d3831bcd8fcb1453812ff13f3de2c6df1d7a45b
janmichek <jan@janmichek.cz>
2024-10-08 13:15:51 +0400

    fix indent

--- src/stores/tokenDetails.js
+++ src/stores/tokenDetails.js
@@ -8,100 +8,99 @@ import { useWalletStore } from '@/stores/wallet'
 import { useDexStore } from '@/stores/dex'
 
 export const useTokenDetailsStore = defineStore('tokenDetails', () => {
-    const { MIDDLEWARE_URL } = useRuntimeConfig().public
-    const axios = useAxios()
+  const { MIDDLEWARE_URL } = useRuntimeConfig().public
+  const axios = useAxios()
   const { aeSdk } = storeToRefs(useWalletStore())
-    const { fetchPrice } = useDexStore()
+  const { fetchPrice } = useDexStore()
 
-    const tokenId = ref(null)
-    const price = ref(null)
-    const rawTokenEvents = ref(null)
-    const tokenEventsCount = ref(null)
+  const tokenId = ref(null)
+  const price = ref(null)
+  const rawTokenEvents = ref(null)
+  const tokenEventsCount = ref(null)
 
-    const rawToken = ref(null)
-    const rawTotalSupply = ref(null)
-    const rawTokenHolders = ref(null)
+  const rawToken = ref(null)
+  const rawTotalSupply = ref(null)
+  const rawTokenHolders = ref(null)
   const tokenHoldersCount = ref(null)
-    const rawTokenTrades = ref(null)
+  const rawTokenTrades = ref(null)
 
-    const tokenDetails = computed(() => rawToken.value
-      ? adaptTokenDetails(
-        rawToken.value,
-        rawTotalSupply.value,
-        price.value,
+  const tokenDetails = computed(() => rawToken.value
+    ? adaptTokenDetails(
+      rawToken.value,
+      rawTotalSupply.value,
+      price.value,
+    )
+    : null,
+  )
+
+  const tokenHolders = computed(() =>
+    tokenDetails.value && rawTokenHolders.value
+      ? adaptTokenHolders(
+        rawTokenHolders.value,
+        tokenDetails.value,
       )
       : null,
-    )
+  )
 
-    const tokenHolders = computed(() =>
-      tokenDetails.value && rawTokenHolders.value
-        ? adaptTokenHolders(
-          rawTokenHolders.value,
-          tokenDetails.value,
-        )
-        : null,
-    )
+  const tokenEvents = computed(() => {
+    return rawTokenEvents.value
+      ? adaptTokenEvents(rawTokenEvents.value)
+      : null
+  })
 
-    const tokenEvents = computed(() => {
-      return rawTokenEvents.value
-        ? adaptTokenEvents(rawTokenEvents.value)
-        : null
-    })
+  function fetchTokenDetails(id) {
+    tokenId.value = id
 
-    function fetchTokenDetails(id) {
-      tokenId.value = id
+    const tokenPromise = fetchToken()
 
-      const tokenPromise = fetchToken()
-
-      return Promise.all([
-        tokenPromise,
+    return Promise.all([
+      tokenPromise,
       Promise.allSettled([
-          fetchTotalSupply(),
-          tokenPromise.then(() => fetchTokenPrice()),
+        fetchTotalSupply(),
+        tokenPromise.then(() => fetchTokenPrice()),
       ]),
-      ])
-    }
+    ])
+  }
 
-    // todo fix indent
-    // todo shorten naming
-    const trades = computed(() => rawTokenTrades.value && price.value
-      ? adaptTrades(rawTokenTrades.value, price.value)
-      : null,
-    )
+  // todo shorten naming
+  const trades = computed(() => rawTokenTrades.value && price.value
+    ? adaptTrades(rawTokenTrades.value, price.value)
+    : null,
+  )
 
-    async function fetchTokenPrice() {
-      price.value = await fetchPrice(tokenId.value, rawToken.value.decimals)
-    }
+  async function fetchTokenPrice() {
+    price.value = await fetchPrice(tokenId.value, rawToken.value.decimals)
+  }
 
-    async function fetchToken() {
-      const { data } = await axios.get(`${MIDDLEWARE_URL}/v3/aex9/${tokenId.value}`)
-      rawToken.value = data
+  async function fetchToken() {
+    const { data } = await axios.get(`${MIDDLEWARE_URL}/v3/aex9/${tokenId.value}`)
+    rawToken.value = data
     tokenEventsCount.value = data.logsCount
-    }
+  }
 
-    async function fetchTotalSupply() {
+  async function fetchTotalSupply() {
     const contract = await Contract.initialize({
       ...aeSdk.value.getContext(),
-        aci: TOKEN_SUPPLY_ACI,
-        address: tokenId.value,
-      })
+      aci: TOKEN_SUPPLY_ACI,
+      address: tokenId.value,
+    })
     const contractCallResult = await contract.total_supply()
-      rawTotalSupply.value = contractCallResult?.decodedResult
-    }
+    rawTotalSupply.value = contractCallResult?.decodedResult
+  }
 
-    async function fetchTokenEvents({ queryParameters, limit, contractId } = {}) {
-      rawTokenEvents.value = null
-      const defaultParameters = `/v3/contracts/logs?contract=${contractId}&aexn-args=true&limit=${limit ?? 10}`
-      const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
-      rawTokenEvents.value = data
-    }
+  async function fetchTokenEvents({ queryParameters, limit, contractId } = {}) {
+    rawTokenEvents.value = null
+    const defaultParameters = `/v3/contracts/logs?contract=${contractId}&aexn-args=true&limit=${limit ?? 10}`
+    const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
+    rawTokenEvents.value = data
+  }
 
-    async function fetchTokenHolders({ queryParameters, limit } = {}) {
-      rawTokenHolders.value = null
+  async function fetchTokenHolders({ queryParameters, limit } = {}) {
+    rawTokenHolders.value = null
     const defaultParameters = `/v3/aex9/${tokenId.value}/balances?by=amount&limit=${limit ?? 10}`
-      const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
-      rawTokenHolders.value = data
-    }
+    const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
+    rawTokenHolders.value = data
+  }
 
   async function fetchTokenHoldersCount() {
     tokenHoldersCount.value = null
@@ -109,17 +108,17 @@ export const useTokenDetailsStore = defineStore('tokenDetails', () => {
     tokenHoldersCount.value = data.holders
   }
 
-    return {
-      fetchTokenDetails,
-      fetchTokenHolders,
+  return {
+    fetchTokenDetails,
+    fetchTokenHolders,
     tokenHoldersCount,
-      fetchTokenEvents,
+    fetchTokenEvents,
       fetchTokenHoldersCount,
-      fetchTokenTrades,
-      tokenDetails,
-      tokenHolders,
-      tokenEvents,
-      tokenEventsCount,
-      trades
-    }
+    fetchTokenTrades,
+    tokenDetails,
+    tokenHolders,
+    tokenEvents,
+    tokenEventsCount,
+    trades,
+  }
 })

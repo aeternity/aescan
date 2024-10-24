36b0a62897071d0af27a83a92e0788c5993b88a5
janmichek <jan@janmichek.cz>
2024-10-24 19:23:22 +0200

    fix name access test

--- cypress/e2e/app/nameDetail.cy.js
+++ cypress/e2e/app/nameDetail.cy.js
@@ -7,6 +7,14 @@ describe('name detail', () => {
     cy.get('.name-pointers-custom-panel table').should('be.visible')
     cy.get('.name-history-panel .paginated-content').should('be.visible')
   })
+  it('should display name detail accessed by name hash', () => {
+    cy.visit(`/names/${Cypress.env('nameAddress')}`)
+
+    cy.get('.name-details-panel table').should('be.visible')
+    cy.get('.name-pointers-special-panel table').should('be.visible')
+    cy.get('.name-pointers-custom-panel table').should('be.visible')
+    cy.get('.name-history-panel .paginated-content').should('be.visible')
+  })
 
   it('should display name does not exist page', () => {
     cy.visit('/names/wrong-harp9e8zcu3qadhrey9p',

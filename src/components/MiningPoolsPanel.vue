2e7a0b2c8cbd5d20e43f85ca551c18968f726833
janmichek <jan@janmichek.cz>
2024-11-08 16:17:05 +0100

    separate to components

--- src/components/MiningPoolsPanel.vue
+++ src/components/MiningPoolsPanel.vue
@@ -1,32 +1,9 @@
 <template>
-  <!--  todo mobile table-->
-  <app-panel class="">
-    <table>
-      <thead>
-        <tr>
-          <th>
-            Name
-          </th>
-          <th>
-            Pool Url
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="miner in MINERS"
-          :key="miner.name">
-          <td>
-            {{ miner.name }}
-          </td>
-          <td>
-            <app-link :to="miner.url">
-              {{ miner.url }}
-            </app-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <app-panel>
+    <!--    todo count-->
+    <mining-pools-table :pools="MINERS"></mining-pools-table>
+    <!--    <mining-pools-table-condensed></mining-pools-table-condensed>-->
+    <!--  todo mobile table-->
   </app-panel>
 </template>
 

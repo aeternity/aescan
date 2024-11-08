2e5b2c5b0733ea31bd38ac7b105b845e2c04383a
janmichek <jan@janmichek.cz>
2024-11-08 16:17:05 +0100

    separate to components

---
+++ src/components/MiningPoolsTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>
+          Name
+          <hint-tooltip>
+            {{ miningHints.mining }}
+          </hint-tooltip>
+        </th>
+        <th>
+          Pool Url
+          <hint-tooltip>
+            {{ miningHints.mining }}
+          </hint-tooltip>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="pool in pools"
+        :key="pool.name">
+        <td>
+          {{ pool.name }}
+        </td>
+        <td>
+          <app-link :to="pool.url">
+            {{ pool.url }}
+          </app-link>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+// todo fix imports
+import { miningHints } from "../utils/hints/miningHints";
+
+defineProps({
+  pools: {
+    type: Array,
+    required: true,
+  }
+})
+</script>

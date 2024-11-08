020bda940379f4f21e8b7b7e68779b5a42035bf2
janmichek <jan@janmichek.cz>
2024-11-08 13:01:00 +0100

    print miners

--- src/components/MinersStatistics.vue
+++ src/components/MinersStatistics.vue
@@ -1,85 +1,89 @@
 <template>
-  <!--  {{ status }}-->
-  <span class="mining-statistics">
-    <app-panel class="mining-statistics__panel">
+  <div class="mining-statistics parent">
+    <app-panel class="mining-statistics__panel div1">
       <h5>MINERS</h5>
       <span class="mining-statistics__value">
         {{ minersCount }}
       </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div2">
       <h5>PEERS</h5>
       <span class="mining-statistics__value">
         {{ status.peerCount }}
       </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div3">
       <h5>POOLS</h5>
       <span class="mining-statistics__value">
         {{ MINERS.length }}
       </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div9">
       <h5>REWARD</h5>
       <span class="mining-statistics__value">
-        <!--        <price-label
+        <price-label
           :price="formatAettosToAe(blockReward)"
-          :max-digits="2"
-          :has-icon="false"/>-->
-        <price-label :price="formatAettosToAe(blockReward, null)"/>
+          :max-digits="2"/>
       </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div5">
       <h5>DIFFICULTY</h5>
       <span class="mining-statistics__value">
         {{ Math.round(status.difficulty / 1000000000) }}
       </span>
-      KG/s
-
+      <span class="mining-statistics__unit">
+        KG/s
+      </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div6">
       <h5>HASHRATE</h5>
       <span class="mining-statistics__value">
         {{ Math.round(status.hashrate / 1000) }}
       </span>
-      KG/s
-
+      <span class="mining-statistics__unit">
+        KG/s
+      </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div7">
       <h5>BLOCKS MINED</h5>
       <span class="mining-statistics__value">
         {{ formatNumber(status.topBlockHeight) }}
       </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div8">
       <h5>BLOCK TIME</h5>
       <span class="mining-statistics__value">
-        {{ Math.round(blocksPer) }}
-        <!--        todo move to mdw-->
+        {{ Math.round(blocksPerMinute) }}
       </span>
-      min/block
+
+      <span class="mining-statistics__unit">
+        min/block
+      </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div4">
       <h5>MAX TPS</h5>
       <span class="mining-statistics__value">
         {{ maxTPS }}
       </span>
-      tx/s
+
+      <span class="mining-statistics__unit">
+        tx/s
+      </span>
     </app-panel>
 
-    <app-panel class="mining-statistics__panel">
+    <app-panel class="mining-statistics__panel div10">
       <h5>TOP MINERS IN LAST 24 HOURS</h5>
       <pie-chart/>
     </app-panel>
-  </span>
+  </div>
 </template>
 <script setup>
 import { useMinersStore } from '@/stores/miners'
@@ -90,8 +94,9 @@ const {
   minersCount,
   blockReward,
   status,
-  blocksPer,
+  blocksPerMinute,
   maxTPS,
+  miners,
 } = storeToRefs(useMinersStore())
 
 if (process.client) {
@@ -100,26 +105,76 @@ if (process.client) {
 </script>
 
 <style scoped>
-.mining-statistics {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  gap: var(--space-2);
+.parent {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  height: 500px;
   width: 100%;
-  margin-bottom: var(--space-2);
+}
 
-  @media (--desktop) {
-    flex-direction: row;
-  }
+.div1 {
+  grid-area: 1 / 1 / 2 / 2;
+}
 
-  &__panel {
-    padding: var(--space-4);
-    width: 50%;
+.div2 {
+  grid-area: 1 / 2 / 2 / 3;
+}
 
-    @media (--desktop) {
-      width: 18%;
-    }
-  }
+.div3 {
+  grid-area: 1 / 3 / 2 / 4;
+}
+
+.div4 {
+  grid-area: 3 / 3 / 4 / 4;
+}
+
+.div5 {
+  grid-area: 2 / 1 / 3 / 2;
+}
+
+.div6 {
+  grid-area: 2 / 2 / 3 / 3;
+}
+
+.div7 {
+  grid-area: 2 / 3 / 3 / 4;
+}
+
+.div8 {
+  grid-area: 3 / 1 / 4 / 2;
+}
+
+.div9 {
+  grid-area: 3 / 2 / 4 / 3;
+}
+
+.div10 {
+  grid-area: 1 / 4 / 4 / 5;
+}
+
+.mining-statistics {
+  /*display: flex;*/
+  /*flex-direction: column;*/
+  /*flex-wrap: wrap;*/
+  /*gap: var(--space-2);*/
+  /*width: 100%;*/
+  /*margin-bottom: var(--space-2);*/
+
+  /*@media (--desktop) {*/
+  /*  flex-direction: row;*/
+  /*}*/
+
+  /*&__panel {*/
+  /*  padding: var(--space-4);*/
+  /*  width: 50%;*/
+
+  /*  @media (--desktop) {*/
+  /*    width: 18%;*/
+  /*  }*/
+  /*}*/
 
   &__value {
     display: inline-flex;
@@ -128,6 +183,10 @@ if (process.client) {
     font-family: var(--font-monospaced);
     font-weight: 400;
     margin-top: var(--space-3);
+  }
+
+  &__unit {
+    white-space: nowrap;
   }
 }
 </style>

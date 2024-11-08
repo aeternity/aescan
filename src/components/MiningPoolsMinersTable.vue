2bf5d4d73ff6dff37e32f5207ae4bb7a8795610a
janmichek <jan@janmichek.cz>
2024-11-08 14:24:55 +0100

    move data to tabs

--- src/components/MiningPoolsMinersTable.vue
+++ src/components/MiningPoolsMinersTable.vue
@@ -2,19 +2,32 @@
   <table>
     <thead>
       <tr>
-        <td>Miner</td>
-        <td>Total Reward</td>
+        <th>Miner
+          <hint-tooltip>
+            {{ miningHints.mining }}
+          </hint-tooltip>
+        </th>
+        <th>Total Reward
+          <hint-tooltip>
+            {{ miningHints.mining }}
+          </hint-tooltip>
+        </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="miner in miners.data">
+      <tr
+        v-for="miner in miners.data"
+        :key="miner.name">
         <td>{{ miner.miner }}</td>
         <td>{{ miner.totalReward }}</td>
       </tr>
     </tbody>
   </table>
 </template>
+
 <script setup>
+import { miningHints } from "../utils/hints/miningHints";
+
 defineProps({
   miners: {
     type: Array,

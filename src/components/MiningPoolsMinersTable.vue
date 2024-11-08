6691eb66dfcb518cff7e75131144c0e0543b2090
janmichek <jan@janmichek.cz>
2024-11-08 14:13:40 +0100

    format miners data

---
+++ src/components/MiningPoolsMinersTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <td>Miner</td>
+        <td>Total Reward</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="miner in miners.data">
+        <td>{{ miner.miner }}</td>
+        <td>{{ miner.totalReward }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script setup>
+defineProps({
+  miners: {
+    type: Array,
+    required: true,
+  },
+})
+</script>

b1d55d27ac75e4fb7684f231999bea83fe7124b8
janmichek <jan@janmichek.cz>
2024-11-08 13:01:00 +0100

    print miners

---
+++ src/components/MiningPoolsMinersPanel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <td>Miner</td>
+          <td>Total Reward</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="miner in miners.data">
+          <td>{{ miner.miner }}</td>
+          <td>{{ miner.totalReward }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { useMinersStore } from '~/stores/miners'
+const { fetchMiners } = useMinersStore()
+const { miners } = storeToRefs(useMinersStore())
+
+if (process.client) {
+  await fetchMiners()
+}
+</script>

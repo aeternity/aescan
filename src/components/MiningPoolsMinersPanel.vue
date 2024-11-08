fcddc3d649adfe145325c173432131ad723806ae
janmichek <jan@janmichek.cz>
2024-11-08 14:13:40 +0100

    format miners data

--- src/components/MiningPoolsMinersPanel.vue
+++ src/components/MiningPoolsMinersPanel.vue
@@ -1,28 +1,46 @@
 <template>
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <td>Miner</td>
-          <td>Total Reward</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="miner in miners.data">
-          <td>{{ miner.miner }}</td>
-          <td>{{ miner.totalReward }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+<!--  todo count-->
+  <app-panel class="mining-pools-miners-panel">
+    <paginated-content
+      :entities="miners"
+      pagination-style="history"
+      @prev-clicked="loadPrevEvents"
+      @next-clicked="loadNextEvents">
+      <mining-pools-miners-table
+        :miners="miners"
+        class="mining-pools-miners-panel__table u-hidden-mobile"/>
+<!--      <mining-pools-miners-table-condensed-->
+<!--        :miners="miners"-->
+<!--        class="u-hidden-desktop"/>-->
+    </paginated-content>
+  </app-panel>
 </template>
 
 <script setup>
-import { useMinersStore } from '~/stores/miners'
-const { fetchMiners } = useMinersStore()
+import { storeToRefs } from 'pinia'
+import { useMinersStore } from '@/stores/miners'
+
 const { miners } = storeToRefs(useMinersStore())
+const { fetchMiners } = useMinersStore()
+
+function loadPrevEvents() {
+  fetchMiners({ queryParameters: miners.value.prev })
+}
+
+function loadNextEvents() {
+  fetchMiners({ queryParameters: miners.value.next })
+}
 
 if (process.client) {
-  await fetchMiners()
+  const limit = computed(() => isDesktop() ? 10 : 3)
+  await fetchMiners({
+    limit: limit.value,
+  })
 }
 </script>
+
+<style scoped>
+.mining-pools-miners-panel-panel__table {
+  margin-bottom: var(--space-4);
+}
+</style>

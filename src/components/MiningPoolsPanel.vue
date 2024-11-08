aa7b4463203c5b6625a6d840e02bc99e9c5b0d23
janmichek <jan@janmichek.cz>
2024-11-08 15:05:03 +0100

    format miner values

---
+++ src/components/MiningPoolsPanel.vue
@@ -0,0 +1,35 @@
+<template>
+<!--  todo mobile table-->
+  <app-panel class="">
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Pool Url
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="miner in MINERS"
+          :key="miner.name">
+          <td>
+            {{ miner.name }}
+          </td>
+          <td>
+            <app-link :to="miner.url">
+              {{ miner.url }}
+            </app-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </app-panel>
+</template>
+
+<script setup>
+
+</script>

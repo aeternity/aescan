e16bf2da82208da812b39828df2c566e2d57658c
Michele Franchi <michele.franchi933@gmail.com>
2024-12-31 09:55:44 +0100

    fix: token symbol

--- src/components/TokenSymbolIcon.vue
+++ src/components/TokenSymbolIcon.vue
@@ -1,22 +1,9 @@
 <template>
-  <!-- <img
-    v-if="isAe"
-    alt="æ token"
-    src="@/assets/ae-logo.svg">
-
-  <img
-    v-else-if="isLaex"
-    alt="LAEX token"
-    src="@/assets/laex-token.svg"> -->
-
   <app-identicon
     :hash="contractId"/>
 </template>
 <script setup>
 import { useRuntimeConfig } from 'nuxt/app'
-/* import { LAEX_CONTRACT_ID } from '@/utils/constants'
-
-const { AE_TOKEN_ID } = useRuntimeConfig().public */
 
 const props = defineProps({
   contractId: {
@@ -24,8 +11,4 @@ const props = defineProps({
     required: true,
   },
 })
-
-/* const isAe = computed(() => props.contractId === AE_TOKEN_ID)
-
-const isLaex = computed(() => props.contractId === LAEX_CONTRACT_ID) */
 </script>

1f72e568bedf94f8166c98440244510b2a478037
Michele Franchi <michele.franchi933@gmail.com>
2025-01-08 15:40:52 +0100

    fix: routes env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,10 +67,10 @@ export default defineNuxtConfig({
   },
   routeRules: Object.fromEntries(
     Object.entries({
-      '/dex-trades': !!process.env.NUXT_PUBLIC_DEX_BACKEND_URL,
-      '/nodes': !!process.env.NUXT_PUBLIC_NODES_BACKEND_URL,
-      '/contract-verification/**': !!process.env.NUXT_PUBLIC_CONTRACT_VERIFICATION_SERVICE_URL,
-      '/tokens/AE': process.env.NUXT_PUBLIC_ENABLE_MARKET_STATS === 'true',
+      '/dex-trades': !!process.env.DEX_BACKEND_URL,
+      '/nodes': !!process.env.NODES_BACKEND_URL,
+      '/contract-verification/**': !!process.env.CONTRACT_VERIFICATION_SERVICE_URL,
+      '/tokens/AE': process.env.ENABLE_MARKET_STATS === 'true',
     }).map(([route, isEnabled]) => [
       route,
       isEnabled ? {} : { redirect: '/' },

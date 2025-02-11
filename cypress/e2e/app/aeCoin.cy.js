49068e23151bf92676a6c9f56c5ee2d92354fc3d
janmichek <jan@janmichek.cz>
2025-02-11 14:09:13 +0800

    fix test

--- cypress/e2e/app/aeCoin.cy.js
+++ cypress/e2e/app/aeCoin.cy.js
@@ -2,7 +2,7 @@ describe('ae coin', () => {
   it('should display ae token detail', () => {
     cy.visit('/tokens/ae')
 
-    cy.get('.ae-coin-panel table__container').should('be.visible')
+    cy.get('.ae-coin-panel table').should('be.visible')
     cy.get('.ae-coin-transactions-panel .paginated-content').should('be.visible')
 
     cy.contains('.tabs__item', 'Markets').click()

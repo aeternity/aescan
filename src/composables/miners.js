2cb69e8f3465b061427d2b109fbbca76565e9aab
janmichek <jan@janmichek.cz>
2025-02-17 20:09:53 +0800

    move fetching top miners to server side

--- src/composables/miners.js
+++ src/composables/miners.js
@@ -1,22 +1,10 @@
-import { defineStore } from 'pinia'
-import { useRuntimeConfig } from 'nuxt/app'
-import useAxios from '@/composables/useAxios'
-// todo move it to api
-// todo loading
-
 export const useMinersStore = defineStore('miners', () => {
-  const { MIDDLEWARE_URL } = useRuntimeConfig().public
-  const axios = useAxios()
-
-  const minersCount = ref(null)
   const miners = ref(null)
-  const blockReward = ref(null)
-  const status = ref(null)
-  const maxTPS = ref(null)
-  const blocksPerMinute = ref(null)
   const topMiners = ref(null)
   const statistics = ref(null)
+  // todo rename
 
+  // todo loading like txs
   async function fetchMining() {
     statistics.value = null
     statistics.value = await $fetch('/api/mining/statistics')
@@ -31,22 +19,18 @@ export const useMinersStore = defineStore('miners', () => {
 
   async function fetchTopMiners({ queryParameters, limit } = {}) {
     topMiners.value = null
-    const defaultParameters = '/stats/miners/top?interval_by=day&min_start_date=2025-02-11&max_start_date=2025-02-11&limit=10&direction=forward'
-    const { data } = await axios.get(`${MIDDLEWARE_URL}${queryParameters || defaultParameters}`)
-    topMiners.value = data.data
+    topMiners.value = await $fetch('/api/mining/top', {
+      params: { queryParameters, limit },
+    })
   }
 
   return {
     statistics,
     miners,
-    minersCount,
-    status,
-    blockReward,
-    blocksPerMinute,
-    maxTPS,
+    topMiners,
+
     fetchMining,
     fetchMiners,
     fetchTopMiners,
-    topMiners,
   }
 })

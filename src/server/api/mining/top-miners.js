59a9a5f4addacd5f69e5bbe8a5c401995235b9aa
janmichek <jan@janmichek.cz>
2025-02-18 09:13:23 +0800

    refactor components

--- src/server/api/mining/top-miners.js
+++ src/server/api/mining/top-miners.js
@@ -5,12 +5,9 @@ const axios = useAxios()
 
 export default defineEventHandler(async event => {
   const query = getQuery(event)
-  console.log('query', query)
-  const defaultParameters = `/stats/miners/top?interval_by=day&min_start_date=2025-02-11&max_start_date=2025-02-11&${query.limit ?? 10}direction=forward`
+  const defaultParameters = `/stats/miners/top?interval_by=day&min_start_date=2025-02-11&max_start_date=2025-02-11&limit=${query.limit ?? 10}&direction=forward`
   const url = new URL(`${MIDDLEWARE_URL}${query.queryParameters || defaultParameters}`)
-  console.log('url.href', url.href)
   const { data } = await axios.get(url)
   // todo different fetch function
-  console.log('data', data)
   return data.data
 })

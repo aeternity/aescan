fa6fa0497ee763473b357c3a662bf2e8038f1684
janmichek <jan@janmichek.cz>
2025-02-18 11:18:15 +0800

    reuse url builder

--- src/server/api/mining/miners.js
+++ src/server/api/mining/miners.js
@@ -1,13 +1,17 @@
 import useAxios from '@/composables/useAxios'
 
-const { MIDDLEWARE_URL } = useRuntimeConfig().public
 const axios = useAxios()
 
 export default defineEventHandler(async event => {
-  const query = getQuery(event)
+  const { limit, queryParameters } = getQuery(event)
 
-  const defaultParameters = `/stats/miners?limit=${query.limit ?? 10}`
-  const { data } = await axios.get(`${MIDDLEWARE_URL}${query.queryParameters || defaultParameters}`)
+  const url = getUrl({
+    entity: 'stats/miners',
+    limit: limit ?? 10,
+    queryParameters,
+  })
+
+  const { data } = await axios.get(url)
 
   return data
 })

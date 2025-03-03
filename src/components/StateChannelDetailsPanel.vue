599967c8ccff94d0fb38c31cfde24978c4aa219d
janmichek <jan@janmichek.cz>
2025-03-03 12:53:05 +0800

    replace style variable

--- src/components/StateChannelDetailsPanel.vue
+++ src/components/StateChannelDetailsPanel.vue
@@ -200,7 +200,7 @@ const stateChannelMiddlewareUrl = `${MIDDLEWARE_URL}/channels/${props.stateChann
     padding-bottom: 0;
 
     @media (--mobile) {
-      padding-bottom: 8px;
+      padding-bottom: var(--space-1);
       width: var(--detail-column-width);
       border-bottom: 1px solid var(--color-midnight-25);
       display: table-cell;
--- src/components/TransactionGeneralPanel.vue
+++ src/components/TransactionGeneralPanel.vue
@@ -182,7 +182,7 @@ const transactionMiddlewareUrl = computed(() => {
     padding-bottom: 0;
 
     @media (--mobile) {
-      padding-bottom: 8px;
+      padding-bottom: var(--space-1);
       width: var(--detail-column-width);
       border-bottom: 1px solid var(--color-midnight-25);
       display: table-cell;

49d6f780172a4e576ed9796fd6be27cf0c70c496
janmichek <jan@janmichek.cz>
2025-03-17 11:52:39 +0800

    refactor kickoff

--- src/composables/nfts.js
+++ src/composables/nfts.js
@@ -1,39 +1,9 @@
-import { useRuntimeConfig } from 'nuxt/app'
-
 export const useNftsStore = defineStore('nfts', () => {
-  const { MIDDLEWARE_URL } = useRuntimeConfig().public
-  const axios = useAxios()
-
-  const rawNfts = ref(null)
-  const nftsCount = ref(null)
-
-  function fetchNfts(queryParameters) {
-    return Promise.allSettled([
-      fetchNftsList(queryParameters),
-      fetchNftsCount(),
-    ])
-  }
-
   const nfts = computed(() => {
     return rawNfts.value
       ? adaptNfts(rawNfts.value)
       : null
   })
-
-  async function fetchNftsList(queryParameters) {
-    rawNfts.value = null
-    const defaultParameters = '/aex141?limit=10&direction=backward&by=creation'
-    const { data } = await axios.get(
-      `${MIDDLEWARE_URL}${queryParameters || defaultParameters}`,
-    )
-    rawNfts.value = data
-  }
-
-  async function fetchNftsCount() {
-    nftsCount.value = null
-    const { data } = await axios.get(`${MIDDLEWARE_URL}/aex141/count`)
-    nftsCount.value = data.data
-  }
 
   return {
     nftsCount,
---
+++ src/server/api/nfts/index.js
@@ -0,0 +1,50 @@
+import useAxios from '@/composables/useAxios'
+
+const axios = useAxios()
+
+export default defineEventHandler(async event => {
+  const { queryParameters } = getQuery(event)
+
+  const [nfts, count] = await Promise.all([
+    fetchNftsList(queryParameters),
+    fetchNftsCount(),
+  ])
+
+  return adaptNfts(nfts, count)
+})
+
+async function fetchNftsList(queryParameters, limit) {
+  const url = getUrl({
+    entity: 'aex141',
+    parameters: { by: 'creation', direction: 'backward' },
+    queryParameters,
+  })
+
+  const { data } = await axios.get(url)
+  return data
+}
+
+async function fetchNftsCount() {
+  const { data } = await axios.get(`${MIDDLEWARE_URL}/aex141/count`)
+  return data.data
+}
+
+function adaptNfts(nfts, count) {
+  const formattedData = nfts.data
+    .map(nft => {
+      return {
+        name: nft.name,
+        blockHeight: nft.blockHeight,
+        creationTime: nft.creationTime,
+        contractId: nft.contractId,
+        nftsAmount: nft.nftsAmount,
+        nftOwners: nft.nftOwners,
+      }
+    })
+  return {
+    next: nfts.next,
+    data: formattedData,
+    prev: nfts.prev,
+    count,
+  }
+}

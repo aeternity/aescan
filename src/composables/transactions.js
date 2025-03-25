5d7836f56edd7d7314d65557c2a1ba71870bb128
janmichek <jan@janmichek.cz>
2025-03-25 17:40:48 +0800

    fix pageindex

--- src/composables/transactions.js
+++ src/composables/transactions.js
@@ -13,7 +13,6 @@ export const useTransactionsStore = defineStore('transactions', () => {
   const { push } = useRouter()
 
   const isHydrated = ref(false)
-  const pageIndex = ref(1)
   const rawTransactions = ref(null)
   const transactionsCount = ref(null)
   const transactionsStatistics = ref(null)
@@ -57,7 +56,6 @@ export const useTransactionsStore = defineStore('transactions', () => {
       fetchTransactions({ queryParameters, scope, type, limit: pageLimit.value }),
       fetchTransactionsCount({ scope, type }),
     ])
-    setPageIndex(1)
   }
 
   async function fetchTransactions({ queryParameters, scope, type, limit } = {}) {
@@ -187,7 +185,6 @@ export const useTransactionsStore = defineStore('transactions', () => {
     changeRoute,
     feesTrend,
     isHydrated,
-    pageIndex,
     selectedTxType,
     selectedScope,
     setPageLimit,

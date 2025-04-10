85ba4364b3def92926195415b0654034236edf85
janmichek <jan@janmichek.cz>
2025-04-10 17:52:04 +0800

    add tests

--- cypress/e2e/app/aeCoin.cy.js
+++ cypress/e2e/app/aeCoin.cy.js
@@ -2,6 +2,8 @@ describe('ae coin', () => {
   it('should display ae token detail', () => {
     cy.visit('/tokens/ae')
 
+    cy.get('.price-chart-panel__line-chart canvas').should('be.visible')
+
     cy.get('.ae-coin-panel table').should('be.visible')
     cy.get('.ae-coin-transactions-panel .paginated-content').should('be.visible')
 

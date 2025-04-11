d7e10c5b86940e067054d756a7c3ac4fb381f1f0
janmichek <jan@janmichek.cz>
2025-04-11 16:25:56 +0800

    add tests

--- src/components/MiningPoolsPanel.vue
+++ src/components/MiningPoolsPanel.vue
@@ -1,10 +1,41 @@
 <template>
-  <app-panel>
-    <mining-pools-table :pools="MINERS"/>
+  <app-panel class="mining-pools-panel">
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Name
+            <hint-tooltip>
+              {{ miningHints.miningPoolName }}
+            </hint-tooltip>
+          </th>
+          <th>
+            Pool Url
+            <hint-tooltip>
+              {{ miningHints.miningPoolUrl }}
+            </hint-tooltip>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="pool in MINING_POOLS"
+          :key="pool.name">
+          <td>
+            {{ pool.name }}
+          </td>
+          <td>
+            <app-link :to="pool.url">
+              {{ pool.url }}
+            </app-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </app-panel>
 </template>
 
-<!--todo is this necessary?-->
 <script setup>
-
+import { miningHints } from '@/utils/hints/miningHints'
+import { MINING_POOLS } from "@/utils/constants";
 </script>

1206b92291a0a9cbd1c081c83ab1fb78d93891bc
janmichek <jan@janmichek.cz>
2025-04-17 09:45:04 +0800

    upgrade minor dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,18 +12,31 @@ export default withNuxt({
     //         ignoreStrings: true,
     //         ignoreTemplateLiterals: true,
     // }],
-    'import/extensions': ['error', 'never'],
+
     'no-console': 'off',
     'no-use-before-define': 'off',
     'no-useless-constructor': 'off',
+
+    'no-restricted-imports': ['error', {
+      patterns: ['./'],
+    }],
+
+    'camelcase': ['error', {
+      properties: 'never',
+    }],
+
+
+    'import/extensions': ['error', 'never'],
     'import/no-named-as-default-member': 'off',
     'import/named': 'off',
     'import/no-named-as-default': 'off',
     'import/default': 'off',
 
-    'no-restricted-imports': ['error', {
-      patterns: ['./'],
-    }],
+    'vue/require-v-for-key': 'error',
+    'vue/no-v-html': 0,
+    'vue/multi-word-component-names': 'off',
+    'vue/mustache-interpolation-spacing': ['error', 'always'],
+    'vue/no-multiple-template-root': 'off',
 
     'vue/html-indent': ['error', 2, {
       attribute: 1,
@@ -31,10 +44,6 @@ export default withNuxt({
       closeBracket: 0,
       alignAttributesVertically: true,
     }],
-
-    'vue/multi-word-component-names': 'off',
-    'vue/mustache-interpolation-spacing': ['error', 'always'],
-    'vue/no-multiple-template-root': 'off',
 
     'vue/no-unused-components': ['error', {
       ignoreWhenBindingPresent: false,
@@ -63,13 +72,5 @@ export default withNuxt({
         max: 1,
       },
     }],
-
-    'vue/require-v-for-key': 'error',
-    'vue/no-v-html': 0,
-
-    'camelcase': ['error', {
-      properties: 'never',
-    }],
-
   },
 })

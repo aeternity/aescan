50b374ffc28f0ace0068f05e9426dc96ae26fddb
Ivaylo Badinov <venimus@gmail.com>
2025-10-21 15:55:28 +0300

    fix: proper prefix strips in url builder

--- src/server/utils/urlBuilder.js
+++ src/server/utils/urlBuilder.js
@@ -3,7 +3,17 @@ const { MIDDLEWARE_URL, NODE_URL, PAGINATION_LIMIT } = useRuntimeConfig().public
 export function getUrl({ entity, id, route, parameters, limit = PAGINATION_LIMIT, queryParameters, node }) {
   const domain = node ? NODE_URL : MIDDLEWARE_URL
   if (queryParameters) {
-    return new URL(`${domain}${queryParameters.substr(3)}`)
+    // decode params if encoded
+    queryParameters = decodeURIComponent(queryParameters)
+    // extract the path from domain
+    const path = new URL(domain).pathname
+    // if path is in queryParameters, remove it
+    if (queryParameters.includes(path)) {
+      queryParameters = queryParameters.replace(path, '')
+    }
+    // strip leading '/' if present
+    queryParameters = queryParameters.replace(/^\//, '').replace(/^\?/, '')
+    return new URL(`${domain}/${queryParameters}`)
   } else {
     const slug = `/${entity}${id ? `/${id}` : ''}${route ? `/${route}` : ''}`
     const url = new URL(`${domain}${slug}`)

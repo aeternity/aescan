03a51d668d67ccf1a7fd76ecc6e89f7d9e7955fe
Ivaylo Badinov <632282+venimus@users.noreply.github.com>
2026-04-01 01:08:41 +0300

    feat: add prev/next to tx details

--- src/composables/transactionDetails.js
+++ src/composables/transactionDetails.js
@@ -7,6 +7,7 @@ export const useTransactionDetailsStore = defineStore('transactionDetails', () =
   const contractId = ref(null)
   const rawTransactionDetails = ref(null)
   const transactionTypeData = ref(null)
+  const adjacentTransactions = ref(null)
 
   const { blockHeight } = storeToRefs(useRecentBlocksStore())
 
@@ -48,6 +49,63 @@ export const useTransactionDetailsStore = defineStore('transactionDetails', () =
     }
   }
 
+  async function fetchAdjacentTransactions() {
+    const { blockHash, blockHeight: txBlockHeight, hash: currentTxHash } = rawTransactionDetails.value || {}
+
+    if (!blockHash?.startsWith('mh_')) {
+      adjacentTransactions.value = { prevHash: null, nextHash: null }
+      return
+    }
+
+    try {
+      // Node API returns all txs in MB as a plain array (0-based); find current tx by hash
+      const [{ data: nodeData }, { data: mb }] = await Promise.all([
+        axios.get(`${NODE_URL}/micro-blocks/hash/${blockHash}/transactions`),
+        axios.get(`${MIDDLEWARE_URL}/micro-blocks/${blockHash}`),
+      ])
+      const mbTxs = nodeData.transactions
+      const txIndex = mbTxs.findIndex(t => t.hash === currentTxHash)
+      const { microBlockIndex, prevHash: mbPrevHash } = mb
+
+      let prevHash = txIndex > 0 ? (mbTxs[txIndex - 1]?.hash ?? null) : null
+      let nextHash = txIndex < mbTxs.length - 1 ? (mbTxs[txIndex + 1]?.hash ?? null) : null
+
+      if (prevHash === null && txIndex === 0) {
+        if (mbPrevHash?.startsWith('mh_')) {
+          const { data: prevMbData } = await axios.get(`${NODE_URL}/micro-blocks/hash/${mbPrevHash}/transactions`)
+          prevHash = prevMbData.transactions[prevMbData.transactions.length - 1]?.hash ?? null
+        } else if (txBlockHeight > 0) {
+          const { data: kbMbs } = await axios.get(`${MIDDLEWARE_URL}/key-blocks/${txBlockHeight - 1}/micro-blocks?limit=1&direction=backward`)
+          const lastMb = kbMbs.data?.[0]
+          if (lastMb) {
+            const { data: prevMbData } = await axios.get(`${NODE_URL}/micro-blocks/hash/${lastMb.hash}/transactions`)
+            prevHash = prevMbData.transactions[prevMbData.transactions.length - 1]?.hash ?? null
+          }
+        }
+      }
+
+      if (nextHash === null && txIndex === mbTxs.length - 1) {
+        const { data: kbMbs } = await axios.get(`${MIDDLEWARE_URL}/key-blocks/${txBlockHeight}/micro-blocks?limit=100&direction=forward`)
+        const nextMb = kbMbs.data?.find(m => m.microBlockIndex === microBlockIndex + 1)
+
+        let nextMbHash = nextMb?.hash
+        if (!nextMbHash) {
+          const { data: nextKbMbs } = await axios.get(`${MIDDLEWARE_URL}/key-blocks/${txBlockHeight + 1}/micro-blocks?limit=1&direction=forward`)
+          nextMbHash = nextKbMbs.data?.[0]?.hash ?? null
+        }
+
+        if (nextMbHash) {
+          const { data: nextMbData } = await axios.get(`${NODE_URL}/micro-blocks/hash/${nextMbHash}/transactions`)
+          nextHash = nextMbData.transactions[0]?.hash ?? null
+        }
+      }
+
+      adjacentTransactions.value = { prevHash, nextHash }
+    } catch {
+      adjacentTransactions.value = { prevHash: null, nextHash: null }
+    }
+  }
+
   async function fetchTimeByBlockHash(blockHash) {
     const { data } = await axios.get(`${NODE_URL}/micro-blocks/hash/${blockHash}/header`)
     return data.time
@@ -61,6 +119,7 @@ export const useTransactionDetailsStore = defineStore('transactionDetails', () =
   function clearTransactionDetails() {
     rawTransactionDetails.value = null
     transactionTypeData.value = null
+    adjacentTransactions.value = null
   }
 
   function updateTransactionTypeData(websocketMessage) {
@@ -71,7 +130,9 @@ export const useTransactionDetailsStore = defineStore('transactionDetails', () =
     contractId,
     transactionDetails,
     transactionTypeData,
+    adjacentTransactions,
     fetchTransactionDetails,
+    fetchAdjacentTransactions,
     fetchContractIdByAccountId,
     updateTransactionTypeData,
   }
